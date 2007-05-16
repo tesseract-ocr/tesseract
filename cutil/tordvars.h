ef28c9b9c0fce844a798f9f6dbdf9135936df757
theraysmith <theraysmith@d0cd1f9f-072b-0410-8dd7-cf729c803f20>
2007-05-16 21:34:24 +0000

    Fixed windows build
    git-svn-id: https://tesseract-ocr.googlecode.com/svn/trunk@59 d0cd1f9f-072b-0410-8dd7-cf729c803f20

--- cutil/tordvars.h
+++ cutil/tordvars.h
@@ -45,7 +45,8 @@ extern int similarity_enable;               /* Switch for Similarity */
 extern int similarity_debug;                /* Level of debug output */
 extern int write_raw_output;                /* Text before context */
 extern int write_output;                    /* Text file output */
-extern "C" { extern int display_ratings; }  /* Show the ratings */
+//extern "C" { extern int display_ratings; }  /* Show the ratings */
+extern int display_ratings;                 /* Show the ratings */
 extern int show_bold;                       /* Use bold text */
 extern int display_text;                    /* Show word text */
 extern int display_blocks;                  /* Show word as boxes */
