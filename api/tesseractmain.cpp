0d81f4b6494de09b01023dbf266b22cbcdf91535
theraysmith <theraysmith@d0cd1f9f-072b-0410-8dd7-cf729c803f20>
2011-03-18 21:43:38 +0000

    Fixed problem that was preventing pagesegmode from being set by config file
    git-svn-id: https://tesseract-ocr.googlecode.com/svn/trunk@554 d0cd1f9f-072b-0410-8dd7-cf729c803f20

--- api/tesseractmain.cpp
+++ api/tesseractmain.cpp
@@ -102,9 +102,9 @@ int main(int argc, char **argv) {
   tesseract::TessBaseAPI  api;
 
   api.SetOutputName(output);
+  api.SetPageSegMode(pagesegmode);
   api.Init(argv[0], lang, tesseract::OEM_DEFAULT,
            &(argv[arg]), argc - arg, false);
-  api.SetPageSegMode(pagesegmode);
 
   tprintf(_("Tesseract Open Source OCR Engine v%s with Leptonica\n"),
            tesseract::TessBaseAPI::Version());
