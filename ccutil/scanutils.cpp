657722aeca31bb376fa8e6869c7ac0b6dd325999
zdenop@gmail.com <zdenop@gmail.com@d0cd1f9f-072b-0410-8dd7-cf729c803f20>
2012-03-01 23:19:35 +0000

    added missing changes for r686
    git-svn-id: https://tesseract-ocr.googlecode.com/svn/trunk@687 d0cd1f9f-072b-0410-8dd7-cf729c803f20

--- ccutil/scanutils.cpp
+++ ccutil/scanutils.cpp
@@ -19,8 +19,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifdef EMBEDDED
-
 #include <ctype.h>
 #include <stdarg.h>
 #include <stddef.h>
