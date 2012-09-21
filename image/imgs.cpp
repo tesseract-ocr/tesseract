b301d39e2ba649d629f58ca200d2aaa7e8e3f913
theraysmith@gmail.com <theraysmith@gmail.com@d0cd1f9f-072b-0410-8dd7-cf729c803f20>
2012-09-21 15:26:59 +0000

    Added 16 bit as a valid image depth
    git-svn-id: https://tesseract-ocr.googlecode.com/svn/trunk@749 d0cd1f9f-072b-0410-8dd7-cf729c803f20

--- image/imgs.cpp
+++ image/imgs.cpp
@@ -246,10 +246,11 @@ inT32 check_legal_image_size(                     //get rest of image
     BADIMAGESIZE.error ("check_legal_image_size", TESSLOG, "(%d,%d)", x, y);
     return -1;                   //failed
   }
-  if (bits_per_pixel != 1 && bits_per_pixel != 2
-      && bits_per_pixel != 4 && bits_per_pixel != 5
-      && bits_per_pixel != 6 && bits_per_pixel != 8 && bits_per_pixel != 24
-      && bits_per_pixel != 32) {
+  if (bits_per_pixel != 1 && bits_per_pixel != 2 &&
+      bits_per_pixel != 4 && bits_per_pixel != 5 &&
+      bits_per_pixel != 6 && bits_per_pixel != 8 &&
+      bits_per_pixel != 16 && bits_per_pixel != 24 &&
+      bits_per_pixel != 32) {
     BADBPP.error ("check_legal_image_size", TESSLOG, "%d", bits_per_pixel);
     return -1;
   }
