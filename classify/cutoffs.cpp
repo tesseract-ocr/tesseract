312ce37de78aca487ffa84d9bddf2fcdc19dc248
Jim O'Regan <joregan@gmail.com>
2015-07-20 18:29:23 +0100

    missing param; rename one

--- classify/cutoffs.cpp
+++ classify/cutoffs.cpp
@@ -40,8 +40,10 @@ namespace tesseract {
  * and insert them into the Cutoffs array.  Cutoffs are
  * indexed in the array by class id.  Unused entries in the
  * array are set to an arbitrarily high cutoff value.
- * @param Filename name of file containing cutoff definitions
+ * @param CutoffFile name of file containing cutoff definitions
  * @param Cutoffs array to put cutoffs into
+ * @param swap
+ * @param end_offset
  * @return none
  * @note Globals: none
  * @note Exceptions: none
