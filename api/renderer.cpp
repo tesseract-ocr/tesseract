aa6a0b12f98cfabc956fe58cdc9cc4e540a24fc3
Robert Theis <robert.m.theis@gmail.com>
2015-08-12 18:02:35 -0700

    Remove extraneous line feed

--- api/renderer.cpp
+++ api/renderer.cpp
@@ -143,7 +143,7 @@ bool TessHOcrRenderer::BeginDocumentHandler() {
         "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n"
         "    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
         "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" "
-        "lang=\"en\">\n <head>\n  <title>\n");
+        "lang=\"en\">\n <head>\n  <title>");
   AppendString(title());
   AppendString(
       "</title>\n"
