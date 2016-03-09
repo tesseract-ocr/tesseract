b46af6da3143394b9f8ece5cdd752e31d326fcb8
zdenop <zdenop@gmail.com>
2016-03-09 19:19:45 +0100

    fix comment about default PSM

--- ccstruct/publictypes.h
+++ ccstruct/publictypes.h
@@ -153,11 +153,11 @@ enum PageSegMode {
   PSM_AUTO_OSD,       ///< Automatic page segmentation with orientation and
                       ///< script detection. (OSD)
   PSM_AUTO_ONLY,      ///< Automatic page segmentation, but no OSD, or OCR.
-  PSM_AUTO,           ///< Fully automatic page segmentation, but no OSD.
+  PSM_AUTO,           ///< Fully automatic page segmentation, but no OSD. (Default)
   PSM_SINGLE_COLUMN,  ///< Assume a single column of text of variable sizes.
   PSM_SINGLE_BLOCK_VERT_TEXT,  ///< Assume a single uniform block of vertically
                                ///< aligned text.
-  PSM_SINGLE_BLOCK,   ///< Assume a single uniform block of text. (Default.)
+  PSM_SINGLE_BLOCK,   ///< Assume a single uniform block of text.
   PSM_SINGLE_LINE,    ///< Treat the image as a single text line.
   PSM_SINGLE_WORD,    ///< Treat the image as a single word.
   PSM_CIRCLE_WORD,    ///< Treat the image as a single word in a circle.
