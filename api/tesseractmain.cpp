7e98c3343291d0529c4f4201193b4f3aad538911
Stefan Weil <sw@weilnetz.de>
2016-03-17 12:10:02 +0100

    Print help text to stdout instead to stderr
    It is common practice for command line programs to show help text
    on stdout. This seems to be reasonable for Tesseract, too.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- api/tesseractmain.cpp
+++ api/tesseractmain.cpp
@@ -71,7 +71,7 @@ void PrintVersionInfo() {
 }
 
 void PrintUsage(const char* program) {
-  fprintf(stderr,
+  printf(
       "Usage:\n"
       "  %s --help | --help-psm | --version\n"
       "  %s --list-langs [--tessdata-dir PATH]\n"
@@ -105,7 +105,7 @@ void PrintHelpForPSM() {
         #endif
         ;
 
-  fprintf(stderr, "%s", msg);
+  printf("%s", msg);
 }
 
 void PrintHelpMessage(const char* program) {
@@ -123,7 +123,7 @@ void PrintHelpMessage(const char* program) {
       "NOTE: These options must occur before any configfile.\n"
      ;
 
-  fprintf(stderr, "\n%s\n", ocr_options);
+  printf("\n%s\n", ocr_options);
   PrintHelpForPSM();
 
   const char *single_options =
@@ -135,7 +135,7 @@ void PrintHelpMessage(const char* program) {
       "  --print-parameters    Print tesseract parameters to stdout.\n"
       ;
 
-  fprintf(stderr, "\n%s", single_options);
+  printf("\n%s", single_options);
 }
 
 void SetVariablesFromCLArgs(tesseract::TessBaseAPI* api, int argc, char** argv) {
