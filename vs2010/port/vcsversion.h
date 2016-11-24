3dddf37b253de8d0ce13b901173c6a4296e41c49
Zdenko Podobný <zdenop@gmail.com>
2016-11-24 14:18:28 +0100

    Increase version number in VS2010

--- vs2010/port/vcsversion.h
+++ vs2010/port/vcsversion.h
@@ -7,5 +7,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#define GIT_REV "3.04.00"
+#define GIT_REV "4.00.00dev"
 
