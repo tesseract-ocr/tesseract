b04a91093207adf26e71efaf729048404b707efa
Zdenko Podobný <zdenop@gmail.com>
2016-11-25 09:31:22 +0100

    opencl: Fix typo in name of local variable
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    
    Conflicts:
    	opencl/openclwrapper.cpp

--- opencl/openclwrapper.cpp
+++ opencl/openclwrapper.cpp
@@ -3319,20 +3319,20 @@ PERF_COUNT_SUB("readProfileFromFile")
     // cleanup
     // TODO: call destructor for profile object?
 
-      bool overrided = false;
+      bool overridden = false;
       char *overrideDeviceStr = getenv("TESSERACT_OPENCL_DEVICE");
       if (overrideDeviceStr != NULL) {
         int overrideDeviceIdx = atoi(overrideDeviceStr);
         if (overrideDeviceIdx > 0 && overrideDeviceIdx <= profile->numDevices ) {
           printf("[DS] Overriding Device Selection (TESSERACT_OPENCL_DEVICE=%s, %i)\n", overrideDeviceStr, overrideDeviceIdx);
           bestDeviceIdx = overrideDeviceIdx - 1;
-          overrided = true;
+          overridden = true;
         } else {
           printf("[DS] Ignoring invalid TESSERACT_OPENCL_DEVICE=%s ([1,%i] are valid devices).\n", overrideDeviceStr, profile->numDevices);
         }
       }
 
-      if (overrided) {
+      if (overridden) {
         printf("[DS] Overridden Device[%i]: \"%s\" (%s)\n", bestDeviceIdx+1, profile->devices[bestDeviceIdx].oclDeviceName, profile->devices[bestDeviceIdx].type==DS_DEVICE_OPENCL_DEVICE ? "OpenCL" : "Native");
       }
       selectedDevice = profile->devices[bestDeviceIdx];
