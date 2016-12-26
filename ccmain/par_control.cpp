3df54a43181cf5eaa126532af49a6744a5115fed
Zdenko Podobný <zdenop@gmail.com>
2016-12-26 13:44:20 +0100

    remove (fake) OPENMP support

--- ccmain/par_control.cpp
+++ ccmain/par_control.cpp
@@ -18,9 +18,6 @@
 ///////////////////////////////////////////////////////////////////////
 
 #include "tesseractclass.h"
-#ifdef _OPENMP
-#include <omp.h>
-#endif // _OPENMP
 
 namespace tesseract {
 
@@ -53,9 +50,6 @@ void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
   }
   // Pre-classify all the blobs.
   if (tessedit_parallelize > 1) {
-#ifdef _OPENMP
-    #pragma omp parallel for num_threads(10)
-#endif // _OPENMP
     for (int b = 0; b < blobs.size(); ++b) {
       *blobs[b].choices =
           blobs[b].tesseract->classify_blob(blobs[b].blob, "par", White, NULL);
