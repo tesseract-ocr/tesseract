9abbc4c6f350adf5c77f19752bd2097f51fdfabf
Stefan Weil <sw@weilnetz.de>
2017-05-11 19:24:57 +0200

    ccstruct: Fix wrong format string
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ccstruct/imagedata.cpp
+++ ccstruct/imagedata.cpp
@@ -492,8 +492,8 @@ inT64 DocumentData::UnCache() {
   pages_offset_ = -1;
   set_total_pages(-1);
   set_memory_used(0);
-  tprintf("Unloaded document %s, saving %d memory\n", document_name_.string(),
-          memory_saved);
+  tprintf("Unloaded document %s, saving %" PRId64 " memory\n",
+          document_name_.string(), memory_saved);
   return memory_saved;
 }
 
