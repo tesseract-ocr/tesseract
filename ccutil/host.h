e05f4c677d8252846d5dd72cd0f9765dcea10ad9
Stefan Weil <sw@weilnetz.de>
2017-05-17 11:55:00 +0200

    Remove obsolete comments and unused code from ccutil/host.h
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ccutil/host.h
+++ ccutil/host.h
@@ -1,34 +1,7 @@
 /******************************************************************************
- **  Filename:       Host.h
+ **  Filename:       host.h
  **  Purpose:        This is the system independent typedefs and defines
  **  Author:         MN, JG, MD
- **  Version:        5.4.1
- **  History:        11/7/94 MCD received the modification that Lennart made
- **                  to port to 32 bit world and modify this file so that it
- **                  will be shared between platform.
- **                  11/9/94 MCD Make MSW32 subset of MSW. Now MSW means
- **                  MicroSoft Window and MSW32 means the 32 bit worlds
- **                  of MicroSoft Window. Therefore you want the environment
- **                  to be MicroSoft Window and in the 32 bit world -
- **                  _WIN32 must be defined by your compiler.
- **                  11/30/94 MCD Incorporated comments received for more
- **                  readability and the missing typedef for FLOAT.
- **                  12/1/94 MCD Added PFVOID typedef
- **                  5/1/95 MCD. Made many changes based on the inputs.
- **                  Changes:
- **                  1) Rearrange the #ifdef so that there're definitions for
- **                  particular platforms.
- **                  2) Took out the #define for computer and environment
- **                  that developer can uncomment
- **                  3) Added __OLDCODE__ where the defines will be
- **                  obsoleted in the next version and advise not to use.
- **                  4) Added the definitions for the following:
- **                  FILE_HANDLE, MEMORY_HANDLE, BOOL8,
- **                  MAX_INT8, MAX_INT16, MAX_INT32, MAX_UINT8
- **                  MAX_UINT16, MAX_UINT32, MAX_FLOAT32
- **                 06/19/96 MCD. Took out MAX_FLOAT32
- **                 07/15/96 MCD. Fixed the comments error
- **                 Add back BOOL8.
  **
  **  (c) Copyright Hewlett-Packard Company, 1988-1996.
  ** Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,20 +18,6 @@
 #ifndef TESSERACT_CCUTIL_HOST_H_
 #define TESSERACT_CCUTIL_HOST_H_
 
-/******************************************************************************
- **                                IMPORTANT!!!                                                                                                                 **
- **                                                                                                                                                                              **
- ** Defines either _WIN32, __MAC__, __UNIX__, __OS2__, __PM__ to
- ** use the specified definitions indicated below in the preprocessor settings.                                                        **
- **                                                                                                                                                                              **
- ** Also define either  __FarProc__ or  __FarData__  and __MOTO__ to use the
- ** specified definitions indicated below in the preprocessor settings.                                                                        **
- **                                                                                                                                                                             **
- ** If a preprocessor settings is not allow in the compiler that is being use,
- ** then it is recommended that a "platform.h" is created with the definition
- ** of the computer and/or operating system.
- ******************************************************************************/
-
 #include <limits>
 #include "platform.h"
 /* _WIN32 */
@@ -70,34 +29,6 @@
 
 #include <cinttypes>    // PRId32, ...
 #include <cstdint>      // int32_t, ...
-
-/********************************************************/
-/* __MAC__ */
-#ifdef __MAC__
-#include <Types.h>
-/*----------------------------*/
-/*----------------------------*/
-#endif
-/********************************************************/
-#if defined(__UNIX__) || defined( __DOS__ ) || defined(__OS2__) || defined(__PM__)
-/*----------------------------*/
-/* FarProc and FarData */
-/*----------------------------*/
-/*----------------------------*/
-#endif
-/*****************************************************************************
- **
- **                      Standard GHC Definitions
- **
- *****************************************************************************/
-
-#ifdef __MOTO__
-#define __NATIVE__   MOTO
-#else
-#define __NATIVE__   INTEL
-#endif
-
-//typedef HANDLE FD*  PHANDLE;
 
 // definitions of portable data types (numbers and characters)
 typedef int8_t inT8;
