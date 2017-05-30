8c0e414a0b43e49798b4202e8aef37038952477e
Raf Schietekat <rfschtkt@users.noreply.github.com>
2017-05-30 21:23:53 +0200

    RAII: Leptonica: DebugPixa::pixa_

--- ccstruct/debugpixa.h
+++ ccstruct/debugpixa.h
@@ -2,6 +2,7 @@
 #define TESSERACT_CCSTRUCT_DEBUGPIXA_H_
 
 #include "allheaders.h"
+#include "raiileptonica.h"
 
 namespace tesseract {
 
@@ -10,14 +11,12 @@ namespace tesseract {
 class DebugPixa {
  public:
   // TODO(rays) add another constructor with size control.
-  DebugPixa() {
-    pixa_ = pixaCreate(0);
+  DebugPixa() : pixa_(pixaCreate(0)) {
     fonts_ = bmfCreate(nullptr, 14);
   }
   // If the filename_ has been set and there are any debug images, they are
   // written to the set filename_.
   ~DebugPixa() {
-    pixaDestroy(&pixa_);
     bmfDestroy(&fonts_);
   }
 
@@ -28,21 +27,21 @@ class DebugPixa {
     int color = depth < 8 ? 1 : (depth > 8 ? 0x00ff0000 : 0x80);
     Pix* pix_debug = pixAddSingleTextblock( // a new Pix, or nullptr for error (TODO: error handling?)
         const_cast<Pix*>(pix), fonts_, caption, color, L_ADD_BELOW, nullptr);
-    pixaAddPix(pixa_, pix_debug, L_INSERT);
+    pixaAddPix(pixa_.p(), pix_debug, L_INSERT);
   }
 
   // Sets the destination filename and enables images to be written to a PDF
   // on destruction.
   void WritePDF(const char* filename) {
-    if (pixaGetCount(pixa_) > 0) {
-      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, "AllDebugImages", filename);
-      pixaClear(pixa_);
+    if (pixaGetCount(pixa_.p()) > 0) {
+      pixaConvertToPdf(pixa_.p(), 300, 1.0f, 0, 0, "AllDebugImages", filename);
+      pixaClear(pixa_.p());
     }
   }
 
  private:
   // The collection of images to put in the PDF.
-  Pixa* pixa_;
+  PixaPtr pixa_;
   // The fonts used to draw text captions.
   L_Bmf* fonts_;
 };
