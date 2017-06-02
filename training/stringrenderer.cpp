2981c6c5852882c2b3693cdb3c904d97fab82dc1
Raf Schietekat <rfschtkt@users.noreply.github.com>
2017-06-02 09:54:16 +0200

    StringRenderer::pen_color_: int[3]->double[3]

--- training/stringrenderer.cpp
+++ training/stringrenderer.cpp
@@ -101,6 +101,7 @@ StringRenderer::StringRenderer(const string& font_desc, int page_width,
       page_height_(page_height),
       h_margin_(50),
       v_margin_(50),
+      pen_color_{0.0, 0.0, 0.0},
       char_spacing_(0),
       leading_(0),
       vertical_text_(false),
@@ -120,15 +121,12 @@ StringRenderer::StringRenderer(const string& font_desc, int page_width,
       start_box_(0),
       page_(0),
       box_padding_(0),
+      page_boxes_(nullptr),
       total_chars_(0),
       font_index_(0),
       last_offset_(0) {
-  pen_color_[0] = 0.0;
-  pen_color_[1] = 0.0;
-  pen_color_[2] = 0.0;
   set_font(font_desc);
   set_resolution(kDefaultOutputResolution);
-  page_boxes_ = nullptr;
 }
 
 bool StringRenderer::set_font(const string& desc) {
