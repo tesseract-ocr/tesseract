28437398433eb6b867a83efc02554dc56c47131e
Stefan Weil <sw@weilnetz.de>
2017-06-03 10:55:03 +0200

    PangoFontInfo: Remove unused method is_italic
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- training/pango_font_info.h
+++ training/pango_font_info.h
@@ -105,7 +105,6 @@ class PangoFontInfo {
   const string& family_name() const    { return family_name_; }
   // Size in points (1/72"), rounded to the nearest integer.
   int font_size() const { return font_size_; }
-  bool is_italic() const { return is_italic_; }
   FontTypeEnum font_type() const { return font_type_; }
 
   int resolution() const { return resolution_; }
@@ -124,7 +123,6 @@ class PangoFontInfo {
   // Font properties set automatically from parsing the font description name.
   string family_name_;
   int font_size_;
-  bool is_italic_;
   FontTypeEnum font_type_;
   // The Pango description that was used to initialize the instance.
   PangoFontDescription* desc_;
