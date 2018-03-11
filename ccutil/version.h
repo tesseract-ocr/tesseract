4b2bea79a5ebcf110d9a63fd29b6a25ffdea3c3e
Amit D <amitdev2222@gmail.com>
2018-03-11 19:25:35 +0200

    Update TESSERACT_VERSION_STR (#1372)

--- ccutil/version.h
+++ ccutil/version.h
@@ -1,7 +1,7 @@
 #ifndef TESSERACT_CCUTIL_VERSION_H_
 #define TESSERACT_CCUTIL_VERSION_H_
 
-#define TESSERACT_VERSION_STR "4.00.00alpha"
+#define TESSERACT_VERSION_STR "4.0.0-beta.1"
 #define TESSERACT_VERSION 0x040000
 #define MAKE_VERSION(major, minor, patch) \
   (((major) << 16) | ((minor) << 8) | (patch))
