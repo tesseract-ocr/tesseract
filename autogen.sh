tesseract-ocr/tesseract

commit d50769dc01df85c993439b2943bac4d884901441

autogen: Report missing requirements (#1394)
* autogen: Report missing autoconf-archive

autoconf-archive is required, but users often missed that requirement.

The script now detects and reports missing autoconf-archive and removes
the incomplete generated configure script.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

* autogen: Report missing pkg-config

pkg-config is required.

The script now detects and reports missing pkg-config and removes
the incomplete generated configure script.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

autogen.sh
@@ -113,6 +113,20 @@ automake --add-missing --copy --warnings=all || bail_out
 echo "Running autoconf"
 autoconf || bail_out
 
+if grep -q AX_CHECK_COMPILE_FLAG configure; then
+  # The generated configure is invalid because autoconf-archive is unavailable.
+  rm configure
+  echo "Missing autoconf-archive. Check the build requirements."
+  bail_out
+fi
+
+if grep -q PKG_CHECK_MODULES configure; then
+  # The generated configure is invalid because pkg-confg is unavailable.
+  rm configure
+  echo "Missing pkg-config. Check the build requirements."
+  bail_out
+fi
+
 echo ""
 echo "All done."
 echo "To build the software now, do something like:"

commit cd4f855a5574a0cf369f93e2a13ace620514768c

autogen: Fix typo in comment
It was introduced by commit d50769dc01df85c993439b2943bac4d884901441.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

autogen.sh
@@ -121,7 +121,7 @@ if grep -q AX_CHECK_COMPILE_FLAG configure; then
 fi
 
 if grep -q PKG_CHECK_MODULES configure; then
-  # The generated configure is invalid because pkg-confg is unavailable.
+  # The generated configure is invalid because pkg-config is unavailable.
   rm configure
   echo "Missing pkg-config. Check the build requirements."
   bail_out