tesseract-ocr/tesseract

commit bb7bb1f0b82011b891b2da18b5e8fc7263a5de03

Remove old comments for exceptions
Exceptions are no longer used.

Remove also some history comments and fix several comments.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/classify/clusttool.cpp
@@ -219,8 +219,6 @@ float *ReadNFloats(TFile *fp, uint16_t N, float Buffer[]) {
  * @param ParamDesc array of param descriptors to write
  * @return None
  * @note Globals: None
- * @note Exceptions: None
- * @note History: 6/6/89, DSJ, Created.
  */
 void WriteParamDesc(FILE *File, uint16_t N, const PARAM_DESC ParamDesc[]) {
   int i;
@@ -248,8 +246,6 @@ void WriteParamDesc(FILE *File, uint16_t N, const PARAM_DESC ParamDesc[]) {
  * @param Proto prototype to write out
  * @return None
  * @note Globals: None
- * @note Exceptions: None
- * @note History: 6/12/89, DSJ, Created.
  */
 void WritePrototype(FILE *File, uint16_t N, PROTOTYPE *Proto) {
   int i;
@@ -298,8 +294,6 @@ void WritePrototype(FILE *File, uint16_t N, PROTOTYPE *Proto) {
  * @param Array array of floats to write
  * @return None
  * @note Globals: None
- * @note Exceptions: None
- * @note History: 6/6/89, DSJ, Created.
  */
 void WriteNFloats(FILE * File, uint16_t N, float Array[]) {
   for (int i = 0; i < N; i++)
@@ -315,8 +309,6 @@ void WriteNFloats(FILE * File, uint16_t N, float Array[]) {
  * @param ProtoStyle prototype style to write
  * @return None
  * @note Globals: None
- * @note Exceptions: None
- * @note History: 6/8/89, DSJ, Created.
  */
 void WriteProtoStyle(FILE *File, PROTOSTYLE ProtoStyle) {
   switch (ProtoStyle) {
@@ -348,9 +340,7 @@ void WriteProtoStyle(FILE *File, PROTOSTYLE ProtoStyle) {
  * @param WriteInsigProtos TRUE to write out insignificants
  * @note Globals: None
  * @return None
- * @note Exceptions: None
- * @note History: 6/12/89, DSJ, Created.
-*/
+ /
 
 void WriteProtoList(FILE* File, uint16_t N, PARAM_DESC* ParamDesc,
                     LIST ProtoList, bool WriteSigProtos,

src/classify/adaptive.cpp
@@ -44,8 +44,6 @@ using tesseract::TFile;
  * @param ClassId class id to associate with new class
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Thu Mar 14 13:06:09 1991, DSJ, Created.
  */
 void AddAdaptedClass(ADAPT_TEMPLATES Templates,
                      ADAPT_CLASS Class,
@@ -75,8 +73,6 @@ void AddAdaptedClass(ADAPT_TEMPLATES Templates,
  * @param Config  config to be freed
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Thu Mar 14 13:34:23 1991, DSJ, Created.
  */
 void FreeTempConfig(TEMP_CONFIG Config) {
   assert (Config != nullptr);
@@ -105,8 +101,6 @@ void FreePermConfig(PERM_CONFIG Config) {
  * @return Ptr to new class data structure.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Thu Mar 14 12:58:13 1991, DSJ, Created.
  */
 ADAPT_CLASS NewAdaptedClass() {
   ADAPT_CLASS Class;
@@ -156,8 +150,6 @@ namespace tesseract {
  * @return Ptr to new adapted templates.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Fri Mar  8 10:15:28 1991, DSJ, Created.
  */
 ADAPT_TEMPLATES Classify::NewAdaptedTemplates(bool InitFromUnicharset) {
   ADAPT_TEMPLATES Templates;
@@ -210,8 +202,6 @@ void free_adapted_templates(ADAPT_TEMPLATES templates) {
  * @return Ptr to new temp config.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Thu Mar 14 13:28:21 1991, DSJ, Created.
  */
 TEMP_CONFIG NewTempConfig(int MaxProtoId, int FontinfoId) {
   int NumProtos = MaxProtoId + 1;
@@ -237,8 +227,6 @@ TEMP_CONFIG NewTempConfig(int MaxProtoId, int FontinfoId) {
  * @return Ptr to new temporary proto.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Thu Mar 14 13:31:31 1991, DSJ, Created.
  */
 TEMP_PROTO NewTempProto() {
   return (TEMP_PROTO)malloc(sizeof(TEMP_PROTO_STRUCT));
@@ -255,8 +243,6 @@ namespace tesseract {
  * @param Templates adapted templates to print to File
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Wed Mar 20 13:35:29 1991, DSJ, Created.
  */
 void Classify::PrintAdaptedTemplates(FILE *File, ADAPT_TEMPLATES Templates) {
   INT_CLASS IClass;
@@ -294,8 +280,6 @@ void Classify::PrintAdaptedTemplates(FILE *File, ADAPT_TEMPLATES Templates) {
  * @return Ptr to new adapted class.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 14:11:01 1991, DSJ, Created.
  */
 ADAPT_CLASS ReadAdaptedClass(TFile *fp) {
   int NumTempProtos;
@@ -347,8 +331,6 @@ namespace tesseract {
  * @return Ptr to adapted templates read from file.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Mon Mar 18 15:18:10 1991, DSJ, Created.
  */
 ADAPT_TEMPLATES Classify::ReadAdaptedTemplates(TFile *fp) {
   ADAPT_TEMPLATES Templates;
@@ -379,8 +361,6 @@ ADAPT_TEMPLATES Classify::ReadAdaptedTemplates(TFile *fp) {
  * @return Ptr to new permanent configuration description.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 14:25:26 1991, DSJ, Created.
  */
 PERM_CONFIG ReadPermConfig(TFile *fp) {
   PERM_CONFIG Config = (PERM_CONFIG)malloc(sizeof(PERM_CONFIG_STRUCT));
@@ -405,8 +385,6 @@ PERM_CONFIG ReadPermConfig(TFile *fp) {
  * @return Ptr to new temporary configuration description.
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 14:29:59 1991, DSJ, Created.
  */
 TEMP_CONFIG ReadTempConfig(TFile *fp) {
   TEMP_CONFIG Config = (TEMP_CONFIG)malloc(sizeof(TEMP_CONFIG_STRUCT));
@@ -430,8 +408,6 @@ TEMP_CONFIG ReadTempConfig(TFile *fp) {
  * @param NumConfigs  number of configs in Class
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 13:33:51 1991, DSJ, Created.
  */
 void WriteAdaptedClass(FILE *File, ADAPT_CLASS Class, int NumConfigs) {
   int NumTempProtos;
@@ -476,8 +452,6 @@ namespace tesseract {
  * @param Templates set of adapted templates to write to File
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Mon Mar 18 15:07:32 1991, DSJ, Created.
  */
 void Classify::WriteAdaptedTemplates(FILE *File, ADAPT_TEMPLATES Templates) {
   int i;
@@ -506,8 +480,6 @@ void Classify::WriteAdaptedTemplates(FILE *File, ADAPT_TEMPLATES Templates) {
  * @param Config  permanent config to write to File
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 13:55:44 1991, DSJ, Created.
  */
 void WritePermConfig(FILE *File, PERM_CONFIG Config) {
   uint8_t NumAmbigs = 0;
@@ -530,8 +502,6 @@ void WritePermConfig(FILE *File, PERM_CONFIG Config) {
  * @param Config  temporary config to write to File
  *
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Tue Mar 19 14:00:28 1991, DSJ, Created.
  */
 void WriteTempConfig(FILE *File, TEMP_CONFIG Config) {
   assert (Config != nullptr);

src/classify/adaptmatch.cpp
@@ -2,7 +2,6 @@
  ** Filename:    adaptmatch.cpp
  ** Purpose:     High level adaptive matcher.
  ** Author:      Dan Johnson
- ** History:     Mon Mar 11 10:00:10 1991, DSJ, Created.
  **
  ** (c) Copyright Hewlett-Packard Company, 1988.
  ** Licensed under the Apache License, Version 2.0 (the "License");
@@ -182,9 +181,6 @@ namespace tesseract {
  *
  * This routine also performs some simple speckle
  * filtering.
- *
- * @note Exceptions: none
- * @note History: Mon Mar 11 10:00:58 1991, DSJ, Created.
  *
  * @param Blob    blob to be classified
  * @param[out] Choices    List of choices found by adaptive matcher.
@@ -460,9 +456,6 @@ void Classify::LearnPieces(const char* fontname, int start, int length,
  * - #AdaptedTemplates current set of adapted templates
  * - #classify_save_adapted_templates TRUE if templates should be saved
  * - #classify_enable_adaptive_matcher TRUE if adaptive matcher is enabled
- *
- * @note Exceptions: none
- * @note History: Tue Mar 19 14:37:06 1991, DSJ, Created.
  */
 void Classify::EndAdaptiveClassifier() {
   STRING Filename;
@@ -531,7 +524,6 @@ void Classify::EndAdaptiveClassifier() {
  *      BuiltInCutoffsFile    file to get avg. feat per class from
  *      classify_use_pre_adapted_templates
  *                            enables use of pre-adapted templates
- *  @note History: Mon Mar 11 12:49:34 1991, DSJ, Created.
  */
 void Classify::InitAdaptiveClassifier(TessdataManager* mgr) {
   if (!classify_enable_adaptive_matcher)
@@ -658,9 +650,6 @@ void Classify::StartBackupAdaptiveClassifier() {
  * Globals:
  * - #EnableLearning
  * set to TRUE by this routine
- *
- * @note Exceptions: none
- * @note History: Mon Apr 15 16:39:29 1991, DSJ, Created.
  */
 void Classify::SettupPass1() {
   EnableLearning = classify_enable_learning;
@@ -678,9 +667,6 @@ void Classify::SettupPass1() {
  *
  * Globals:
  * - #EnableLearning set to FALSE by this routine
- *
- * @note Exceptions: none
- * @note History: Mon Apr 15 16:39:29 1991, DSJ, Created.
  */
 void Classify::SettupPass2() {
   EnableLearning = FALSE;
@@ -705,9 +691,6 @@ void Classify::SettupPass2() {
  * - #AllProtosOn dummy mask with all 1's
  * - BaselineCutoffs kludge needed to get cutoffs
  * - #PreTrainedTemplates kludge needed to get cutoffs
- *
- * @note Exceptions: none
- * @note History: Thu Mar 14 12:49:39 1991, DSJ, Created.
  */
 void Classify::InitAdaptedClass(TBLOB *Blob,
                                 CLASS_ID ClassId,
@@ -801,8 +784,6 @@ void Classify::InitAdaptedClass(TBLOB *Blob,
  *
  * @return Number of pico-features returned (0 if
  * an error occurred)
- * @note Exceptions: none
- * @note History: Tue Mar 12 17:55:18 1991, DSJ, Created.
  */
 int Classify::GetAdaptiveFeatures(TBLOB *Blob,
                                   INT_FEATURE_ARRAY IntFeatures,
@@ -838,9 +819,7 @@ int Classify::GetAdaptiveFeatures(TBLOB *Blob,
  *
  * @param word current word
  *
- * @return TRUE or FALSE
- * @note Exceptions: none
- * @note History: Thu May 30 14:25:06 1991, DSJ, Created.
+ * @return true or false
  */
 bool Classify::AdaptableWord(WERD_RES* word) {
   if (word->best_choice == nullptr) return false;
@@ -875,8 +854,6 @@ bool Classify::AdaptableWord(WERD_RES* word) {
  * - AllConfigsOn dummy mask to match against all configs
  *
  * @return none
- * @note Exceptions: none
- * @note History: Thu Mar 14 09:36:03 1991, DSJ, Created.
  */
 void Classify::AdaptToChar(TBLOB* Blob, CLASS_ID ClassId, int FontinfoId,
                            float Threshold,
@@ -1018,9 +995,6 @@ void Classify::DisplayAdaptedChar(TBLOB* blob, INT_CLASS_STRUCT* int_class) {
  *
  * @param new_result new result to add
  * @param[out] results results to add new result to
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 18:19:29 1991, DSJ, Created.
  */
 void Classify::AddNewResult(const UnicharRating& new_result,
                             ADAPT_RESULTS *results) {
@@ -1072,9 +1046,6 @@ void Classify::AddNewResult(const UnicharRating& new_result,
  * @param[out] results place to put match results
  * @param int_features
  * @param fx_info
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 19:40:36 1991, DSJ, Created.
  */
 void Classify::AmbigClassifier(
     const GenericVector<INT_FEATURE_STRUCT>& int_features,
@@ -1295,8 +1266,6 @@ double Classify::ComputeCorrectedRating(bool debug, int unichar_id,
  * @param fx_info
  *
  * @return Array of possible ambiguous chars that should be checked.
- * @note Exceptions: none
- * @note History: Tue Mar 12 19:38:03 1991, DSJ, Created.
  */
 UNICHAR_ID *Classify::BaselineClassifier(
     TBLOB *Blob, const GenericVector<INT_FEATURE_STRUCT>& int_features,
@@ -1343,9 +1312,6 @@ UNICHAR_ID *Classify::BaselineClassifier(
  * - CharNormCutoffs expected num features for each class
  * - AllProtosOn mask that enables all protos
  * - AllConfigsOn mask that enables all configs
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 16:02:52 1991, DSJ, Created.
  */
 int Classify::CharNormClassifier(TBLOB *blob,
                                  const TrainingSample& sample,
@@ -1434,9 +1400,6 @@ int Classify::CharNormTrainingSample(bool pruner_only,
  *
  * Globals:
  * - matcher_avg_noise_size avg. length of a noise blob
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 18:36:52 1991, DSJ, Created.
  */
 void Classify::ClassifyAsNoise(ADAPT_RESULTS *results) {
   float rating = results->BlobLength / matcher_avg_noise_size;
@@ -1535,9 +1498,6 @@ void Classify::ConvertMatchesToChoices(const DENORM& denorm, const TBOX& box,
  * @param Results results of match being debugged
  *
  * Globals: none
- *
- * @note Exceptions: none
- * @note History: Wed Mar 13 16:44:41 1991, DSJ, Created.
  */
 void Classify::DebugAdaptiveClassifier(TBLOB *blob,
                                        ADAPT_RESULTS *Results) {
@@ -1571,9 +1531,6 @@ void Classify::DebugAdaptiveClassifier(TBLOB *blob,
  * - PreTrainedTemplates built-in training templates
  * - AdaptedTemplates templates adapted for this page
  * - matcher_reliable_adaptive_result rating limit for a great match
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 08:50:11 1991, DSJ, Created.
  */
 void Classify::DoAdaptiveMatch(TBLOB *Blob, ADAPT_RESULTS *Results) {
   UNICHAR_ID *Ambiguities;
@@ -1629,8 +1586,6 @@ void Classify::DoAdaptiveMatch(TBLOB *Blob, ADAPT_RESULTS *Results) {
  * - PreTrainedTemplates built-in templates
  *
  * @return String containing all possible ambiguous classes.
- * @note Exceptions: none
- * @note History: Fri Mar 15 08:08:22 1991, DSJ, Created.
  */
 UNICHAR_ID *Classify::GetAmbiguities(TBLOB *Blob,
                                      CLASS_ID CorrectClass) {
@@ -1717,8 +1672,6 @@ bool Classify::LooksLikeGarbage(TBLOB *blob) {
  * Globals:
  *
  * @return Number of features extracted or 0 if an error occurred.
- * @note Exceptions: none
- * @note History: Tue May 28 10:40:52 1991, DSJ, Created.
  */
 int Classify::GetCharNormFeature(const INT_FX_RESULT_STRUCT& fx_info,
                                  INT_TEMPLATES templates,
@@ -1781,8 +1734,6 @@ void Classify::ComputeCharNormArrays(FEATURE_STRUCT* norm_feature,
  *
  * @return The id of the new config created, a negative integer in
  * case of error.
- * @note Exceptions: none
- * @note History: Fri Mar 15 08:49:46 1991, DSJ, Created.
  */
 int Classify::MakeNewTemporaryConfig(ADAPT_TEMPLATES Templates,
                            CLASS_ID ClassId,
@@ -1878,8 +1829,6 @@ int Classify::MakeNewTemporaryConfig(ADAPT_TEMPLATES Templates,
  * Globals: none
  *
  * @return Max proto id in class after all protos have been added.
- * Exceptions: none
- * History: Fri Mar 15 11:39:38 1991, DSJ, Created.
  */
 PROTO_ID Classify::MakeNewTempProtos(FEATURE_SET Features,
                                      int NumBadFeat,
@@ -1966,9 +1915,6 @@ PROTO_ID Classify::MakeNewTempProtos(FEATURE_SET Features,
  * @param Blob current blob being adapted to
  *
  * Globals: none
- *
- * @note Exceptions: none
- * @note History: Thu Mar 14 15:54:08 1991, DSJ, Created.
  */
 void Classify::MakePermanent(ADAPT_TEMPLATES Templates,
                              CLASS_ID ClassId,
@@ -2029,8 +1975,6 @@ void Classify::MakePermanent(ADAPT_TEMPLATES Templates,
  * Globals: none
  *
  * @return TRUE if TempProto is converted, FALSE otherwise
- * @note Exceptions: none
- * @note History: Thu Mar 14 18:49:54 1991, DSJ, Created.
  */
 int MakeTempProtoPerm(void *item1, void *item2) {
   ADAPT_CLASS Class;
@@ -2064,9 +2008,6 @@ namespace tesseract {
  * @param results match results to write to File
  *
  * Globals: none
- *
- * @note Exceptions: none
- * @note History: Mon Mar 18 09:24:53 1991, DSJ, Created.
  */
 void Classify::PrintAdaptiveMatchResults(const ADAPT_RESULTS& results) {
   for (int i = 0; i < results.match.size(); ++i) {
@@ -2087,9 +2028,6 @@ void Classify::PrintAdaptiveMatchResults(const ADAPT_RESULTS& results) {
  *
  * Globals:
  * - matcher_bad_match_pad defines a "bad match"
- *
- * @note Exceptions: none
- * @note History: Tue Mar 12 13:51:03 1991, DSJ, Created.
  */
 void Classify::RemoveBadMatches(ADAPT_RESULTS *Results) {
   int Next, NextGood;
@@ -2150,8 +2088,6 @@ void Classify::RemoveBadMatches(ADAPT_RESULTS *Results) {
  * present.
  *
  * @param Results contains matches to be filtered
- *
- * @note History: Tue Mar 12 13:51:03 1991, DSJ, Created.
  */
 void Classify::RemoveExtraPuncs(ADAPT_RESULTS *Results) {
   int Next, NextGood;
@@ -2200,9 +2136,6 @@ void Classify::RemoveExtraPuncs(ADAPT_RESULTS *Results) {
  *
  * Globals:
  * - matcher_good_threshold default good match rating
- *
- * @note Exceptions: none
- * @note History: Tue Apr  9 08:33:13 1991, DSJ, Created.
  */
 void Classify::SetAdaptiveThreshold(float Threshold) {
   Threshold = (Threshold == matcher_good_threshold) ? 0.9: (1.0 - Threshold);
@@ -2220,9 +2153,6 @@ void Classify::SetAdaptiveThreshold(float Threshold) {
  * @param shape_id classifier id to work with
  * @param features features of the unknown character
  * @param num_features Number of features in the features array.
- *
- * @note Exceptions: none
- * @note History: Fri Mar 22 08:43:52 1991, DSJ, Created.
  */
 
 void Classify::ShowBestMatchFor(int shape_id,

src/classify/cutoffs.cpp
@@ -42,8 +42,6 @@ namespace tesseract {
  * @param Cutoffs array to put cutoffs into
  * @return none
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Wed Feb 20 09:38:26 1991, DSJ, Created.
  */
 void Classify::ReadNewCutoffs(TFile* fp, CLASS_CUTOFF_ARRAY Cutoffs) {
   char Class[UNICHAR_LEN + 1];

src/classify/float2int.cpp
@@ -40,9 +40,6 @@ namespace tesseract {
  * - none
  *
  * @param char_norm_array array to be cleared
- *
- * @note Exceptions: none
- * @note History: Wed Feb 20 11:20:54 1991, DSJ, Created.
  */
 void Classify::ClearCharNormArray(uint8_t* char_norm_array) {
   memset(char_norm_array, 0, sizeof(*char_norm_array) * unicharset.size());
@@ -61,9 +58,6 @@ void Classify::ClearCharNormArray(uint8_t* char_norm_array) {
  *
  * @param norm_feature character normalization feature
  * @param[out] char_norm_array place to put results of size unicharset.size()
- *
- * @note Exceptions: none
- * @note History: Wed Feb 20 11:20:54 1991, DSJ, Created.
  */
 void Classify::ComputeIntCharNormArray(const FEATURE_STRUCT& norm_feature,
                                        uint8_t* char_norm_array) {
@@ -92,9 +86,6 @@ void Classify::ComputeIntCharNormArray(const FEATURE_STRUCT& norm_feature,
  *
  * @param Features floating point pico-features to be converted
  * @param[out] IntFeatures array to put converted features into
- *
- * @note Exceptions: none
- * @note History: Wed Feb 20 10:58:45 1991, DSJ, Created.
  */
 void Classify::ComputeIntFeatures(FEATURE_SET Features,
                                   INT_FEATURE_ARRAY IntFeatures) {

src/classify/fpoint.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- ** Filename:    fpoint.c
+ ** Filename:    fpoint.cpp
  ** Purpose:     Abstract data type for a 2D point (floating point coords)
  ** Author:      Dan Johnson
  **
@@ -40,8 +40,6 @@ float DistanceBetween(FPOINT A, FPOINT B) {
  * @param FullScale value to associate with 2*pi
  * @return none
  * @note Globals: none
- * @note Exceptions: none
- * @note History: Wed Mar 28 14:27:25 1990, DSJ, Created.
  */
 float NormalizedAngleFrom(FPOINT *Point1, FPOINT *Point2, float FullScale) {
   float NumRadsInCircle = 2.0 * M_PI;

src/classify/intmatcher.cpp
@@ -466,8 +466,6 @@ int Classify::PruneClasses(const INT_TEMPLATES_STRUCT* int_templates,
  * param Result Class rating & configuration: (0.0 -> 1.0), 0=bad, 1=good
  * param Debug Debugger flag: 1=debugger on
  * @return none
- * @note Exceptions: none
- * @note History: Tue Feb 19 16:36:23 MST 1991, RWM, Created.
  */
 void IntegerMatcher::Match(INT_CLASS ClassTemplate,
                            BIT_VECTOR ProtoMask,
@@ -547,8 +545,6 @@ void IntegerMatcher::Match(INT_CLASS ClassTemplate,
  * param AdaptProtoThreshold Threshold for good protos
  * param Debug Debugger flag: 1=debugger on
  * @return Number of good protos in ProtoArray.
- * @note Exceptions: none
- * @note History: Tue Mar 12 17:09:26 MST 1991, RWM, Created
  */
 int IntegerMatcher::FindGoodProtos(
     INT_CLASS ClassTemplate,
@@ -618,7 +614,6 @@ int IntegerMatcher::FindGoodProtos(
  * @param AdaptFeatureThreshold Threshold for bad features
  * @param Debug Debugger flag: 1=debugger on
  * @return Number of bad features in FeatureArray.
- * @note History: Tue Mar 12 17:09:26 MST 1991, RWM, Created
  */
 int IntegerMatcher::FindBadFeatures(
     INT_CLASS ClassTemplate,
@@ -717,8 +712,6 @@ void ScratchEvidence::ClearFeatureEvidence(const INT_CLASS class_template) {
 /**
  * Print debugging information for Configurations
  * @return none
- * @note Exceptions: none
- * @note History: Wed Feb 27 14:12:28 MST 1991, RWM, Created.
  */
 void IMDebugConfiguration(int FeatureNum,
                           uint16_t ActualProtoNum,
@@ -740,8 +733,6 @@ void IMDebugConfiguration(int FeatureNum,
 /**
  * Print debugging information for Configurations
  * @return none
- * @note Exceptions: none
- * @note History: Wed Feb 27 14:12:28 MST 1991, RWM, Created.
  */
 void IMDebugConfigurationSum(int FeatureNum,
                              uint8_t *FeatureEvidence,
@@ -913,8 +904,6 @@ int IntegerMatcher::UpdateTablesForFeature(
 /**
  * Print debugging information for Configurations
  * @return none
- * @note Exceptions: none
- * @note History: Wed Feb 27 14:12:28 MST 1991, RWM, Created.
  */
 #ifndef GRAPHICS_DISABLED
 void IntegerMatcher::DebugFeatureProtoError(
@@ -1174,8 +1163,6 @@ void ScratchEvidence::NormalizeSums(
  * Find the best match for the current class and update the Result
  * with the configuration and match rating.
  * @return The best normalized sum of evidences
- * @note Exceptions: none
- * @note History: Wed Feb 27 14:12:28 MST 1991, RWM, Created.
  */
 int IntegerMatcher::FindBestMatch(
     INT_CLASS class_template,
@@ -1224,8 +1211,6 @@ float IntegerMatcher::ApplyCNCorrection(float rating, int blob_length,
  * @param ra     Key array [1..n]
  * @param rb     Index array [1..n]
  * @return none
- * @note Exceptions: none
- * @note History: Tue Feb 19 10:24:24 MST 1991, RWM, Created.
  */
 void
 HeapSort (int n, int ra[], int rb[]) {

commit f8684cb0fddaa822a530efd5694565552f1e708e

Fix syntax error (regression)
It was introduced in commit bb7bb1f0b82011b891b2da18b5e8fc7263a5de03.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/classify/clusttool.cpp
@@ -340,7 +340,7 @@ void WriteProtoStyle(FILE *File, PROTOSTYLE ProtoStyle) {
  * @param WriteInsigProtos TRUE to write out insignificants
  * @note Globals: None
  * @return None
- /
+ */
 
 void WriteProtoList(FILE* File, uint16_t N, PARAM_DESC* ParamDesc,
                     LIST ProtoList, bool WriteSigProtos,