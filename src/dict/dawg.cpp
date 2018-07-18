dfc3e9691feaf918e822e344ee37b58529797921
Stefan Weil <sw@weilnetz.de>
2018-07-18 19:02:01 +0200

    SquishedDawg: Use new serialization API
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/dict/dawg.cpp
+++ src/dict/dawg.cpp
@@ -315,23 +315,20 @@ bool SquishedDawg::read_squished_dawg(TFile *file) {
   // Read the magic number and check that it matches kDawgMagicNumber, as
   // auto-endian fixing should make sure it is always correct.
   int16_t magic;
-  if (file->FReadEndian(&magic, sizeof(magic), 1) != 1) return false;
+  if (!file->DeSerialize(&magic)) return false;
   if (magic != kDawgMagicNumber) {
     tprintf("Bad magic number on dawg: %d vs %d\n", magic, kDawgMagicNumber);
     return false;
   }
 
   int32_t unicharset_size;
-  if (file->FReadEndian(&unicharset_size, sizeof(unicharset_size), 1) != 1)
-    return false;
-  if (file->FReadEndian(&num_edges_, sizeof(num_edges_), 1) != 1) return false;
+  if (!file->DeSerialize(&unicharset_size)) return false;
+  if (!file->DeSerialize(&num_edges_)) return false;
   ASSERT_HOST(num_edges_ > 0);  // DAWG should not be empty
   Dawg::init(unicharset_size);
 
   edges_ = new EDGE_RECORD[num_edges_];
-  if (file->FReadEndian(&edges_[0], sizeof(edges_[0]), num_edges_) !=
-      num_edges_)
-    return false;
+  if (!file->DeSerialize(&edges_[0], num_edges_)) return false;
   if (debug_level_ > 2) {
     tprintf("type: %d lang: %s perm: %d unicharset_size: %d num_edges: %d\n",
             type_, lang_.string(), perm_, unicharset_size_, num_edges_);
@@ -382,9 +379,8 @@ bool SquishedDawg::write_squished_dawg(TFile *file) {
 
   // Write the magic number to help detecting a change in endianness.
   int16_t magic = kDawgMagicNumber;
-  if (file->FWrite(&magic, sizeof(magic), 1) != 1) return false;
-  if (file->FWrite(&unicharset_size_, sizeof(unicharset_size_), 1) != 1)
-    return false;
+  if (!file->Serialize(&magic)) return false;
+  if (!file->Serialize(&unicharset_size_)) return false;
 
   // Count the number of edges in this Dawg.
   num_edges = 0;
@@ -393,7 +389,7 @@ bool SquishedDawg::write_squished_dawg(TFile *file) {
       num_edges++;
 
   // Write edge count to file.
-  if (file->FWrite(&num_edges, sizeof(num_edges), 1) != 1) return false;
+  if (!file->Serialize(&num_edges)) return false;
 
   if (debug_level_) {
     tprintf("%d nodes in DAWG\n", node_count);
@@ -406,8 +402,7 @@ bool SquishedDawg::write_squished_dawg(TFile *file) {
         old_index = next_node_from_edge_rec(edges_[edge]);
         set_next_node(edge, node_map[old_index]);
         temp_record = edges_[edge];
-        if (file->FWrite(&temp_record, sizeof(temp_record), 1) != 1)
-          return false;
+        if (!file->Serialize(&temp_record)) return false;
         set_next_node(edge, old_index);
       } while (!last_edge(edge++));
 
