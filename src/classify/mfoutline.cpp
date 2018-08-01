d28631a274b97239a83ff1aaa257246a65c322d7
Noah Metzger <noah.metzger@bib.uni-mannheim.de>
2018-08-01 14:29:07 +0200

    Fix issues detected by Coverity Scan
    CID: 1164604 (Nesting level does not match indentation)
    
    Signed-off-by: Noah Metzger <noah.metzger@bib.uni-mannheim.de>

--- src/classify/mfoutline.cpp
+++ src/classify/mfoutline.cpp
@@ -383,48 +383,56 @@ void ComputeDirection(MFEDGEPT *Start,
 
   Delta.x = Finish->Point.x - Start->Point.x;
   Delta.y = Finish->Point.y - Start->Point.y;
-  if (Delta.x == 0)
-  if (Delta.y < 0) {
-    Start->Slope = -FLT_MAX;
-    Start->Direction = south;
-  }
-  else {
-    Start->Slope = FLT_MAX;
-    Start->Direction = north;
-  }
-  else {
+  if (Delta.x == 0) {
+    if (Delta.y < 0) {
+      Start->Slope = -FLT_MAX;
+      Start->Direction = south;
+    } else {
+      Start->Slope = FLT_MAX;
+      Start->Direction = north;
+    }
+  } else {
     Start->Slope = Delta.y / Delta.x;
-    if (Delta.x > 0)
-      if (Delta.y > 0)
-        if (Start->Slope > MinSlope)
-          if (Start->Slope < MaxSlope)
+    if (Delta.x > 0) {
+      if (Delta.y > 0) {
+        if (Start->Slope > MinSlope) {
+          if (Start->Slope < MaxSlope) {
             Start->Direction = northeast;
-    else
-      Start->Direction = north;
-    else
-      Start->Direction = east;
-    else if (Start->Slope < -MinSlope)
-    if (Start->Slope > -MaxSlope)
-      Start->Direction = southeast;
-    else
-      Start->Direction = south;
-    else
-      Start->Direction = east;
-    else if (Delta.y > 0)
-    if (Start->Slope < -MinSlope)
-      if (Start->Slope > -MaxSlope)
-        Start->Direction = northwest;
-    else
-      Start->Direction = north;
-    else
+          } else {
+            Start->Direction = north;
+          }
+        } else {
+          Start->Direction = east;
+        }
+      }
+      else if (Start->Slope < -MinSlope) {
+        if (Start->Slope > -MaxSlope) {
+          Start->Direction = southeast;
+        } else {
+          Start->Direction = south;
+        }
+      } else {
+        Start->Direction = east;
+      }
+    } else if (Delta.y > 0) {
+      if (Start->Slope < -MinSlope) {
+        if (Start->Slope > -MaxSlope) {
+          Start->Direction = northwest;
+        } else {
+          Start->Direction = north;
+        }
+      } else {
+        Start->Direction = west;
+      }  
+    } else if (Start->Slope > MinSlope) {
+      if (Start->Slope < MaxSlope) {
+        Start->Direction = southwest;
+      } else {
+        Start->Direction = south;
+      } 
+    } else {
       Start->Direction = west;
-    else if (Start->Slope > MinSlope)
-    if (Start->Slope < MaxSlope)
-      Start->Direction = southwest;
-    else
-      Start->Direction = south;
-    else
-      Start->Direction = west;
+    }   
   }
   Finish->PreviousDirection = Start->Direction;
 }
