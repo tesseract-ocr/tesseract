68db6aa5df065e8878fbbbf491e08df453048eb1
Stefan Weil <sw@weilnetz.de>
2018-08-25 11:07:32 +0200

    unittest: Fix and enable cleanapi_test
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- unittest/cleanapi_test.cc
+++ unittest/cleanapi_test.cc
@@ -1,6 +1,15 @@
-//
+// (C) Copyright 2017, Google Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include "tesseract/api/baseapi.h"
+#include "baseapi.h"
 
 // Dummy enum in the global namespace that checks for collision with awkward
 // names.
@@ -18,6 +27,9 @@ enum NameTester {
   WORD
 };
 
+#define ERRCODE_H // avoid redefinition of ABORT in errcode.h
+#include "include_gunit.h"
+
 namespace {
 
 // Verifies that the global namespace is clean.
@@ -26,4 +38,3 @@ TEST(CleanNamespaceTess, DummyTest) {
 }
 
 }  // namespace.
-
