e6c9f77af46c3bb7092262290dc2067a05786490
Stefan Weil <sw@weilnetz.de>
2018-10-02 14:18:55 +0200

    Remove "v" prefix for version in banner
    Instead of printing the version with an additional "v" (which leads to
    results like `Tesseract Open Source OCR Engine vv4.0.0-beta.4`),
    just print the version string in the banner text of Tesseract.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/api/tesseractmain.cpp
+++ src/api/tesseractmain.cpp
@@ -333,7 +333,7 @@ static void PrintLangsList(tesseract::TessBaseAPI &api) {
 }
 
 static void PrintBanner() {
-  tprintf("Tesseract Open Source OCR Engine v%s with Leptonica\n",
+  tprintf("Tesseract Open Source OCR Engine %s with Leptonica\n",
           tesseract::TessBaseAPI::Version());
 }
 
