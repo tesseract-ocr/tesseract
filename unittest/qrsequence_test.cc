d3cf423748ebab98e53b9d2d468e8410d845d72c
Stefan Weil <sw@weilnetz.de>
2018-10-12 08:01:58 +0200

    unittest: Add qrsequence_test
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- unittest/qrsequence_test.cc
+++ unittest/qrsequence_test.cc
@@ -1,9 +1,22 @@
-//
+// (C) Copyright 2017, Google Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include "tesseract/ccutil/qrsequence.h"
 
 #include <algorithm>
 #include <vector>
+
+#include "cycletimer.h"
+#include "include_gunit.h"
+#include "log.h"
+#include "qrsequence.h"
 
 namespace {
 
@@ -33,8 +46,7 @@ TEST_P(QRSequenceGeneratorTest, GeneratesValidSequence) {
   CycleTimer timer;
   timer.Restart();
   for (int i = 0; i < kRangeSize; ++i) vals[i] = generator.GetVal();
-  LOG(INFO) << kRangeSize << "-length sequence took " << timer.Get() * 1e3
-            << "ms";
+  LOG(INFO) << kRangeSize << "-length sequence took " << timer.GetInMs() << "ms";
   // Sort the numbers to verify that we've covered the range without repetition.
   std::sort(vals.begin(), vals.end());
   for (int i = 0; i < kRangeSize; ++i) {
