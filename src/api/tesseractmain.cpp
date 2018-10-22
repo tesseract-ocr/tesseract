tesseract-ocr/tesseract

commit 49d7df6dc3efc61861c783021e06567d3d4780f0

tesseractmain: Show error message when output file could not be created
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/api/tesseractmain.cpp
@@ -22,6 +22,7 @@
 #include "config_auto.h"
 #endif
 
+#include <cerrno>               // for errno
 #include <iostream>
 
 #include "allheaders.h"
@@ -33,7 +34,7 @@
 #include "renderer.h"
 #include "simddetect.h"
 #include "strngs.h"
-#include "tprintf.h"
+#include "tprintf.h"            // for tprintf
 
 #if defined(_WIN32)
 #include <fcntl.h>
@@ -407,16 +408,28 @@ static void PreloadRenderers(
     if (b) {
       bool font_info;
       api->GetBoolVariable("hocr_font_info", &font_info);
-      renderers->push_back(
-          new tesseract::TessHOcrRenderer(outputbase, font_info));
+      tesseract::TessHOcrRenderer* renderer =
+          new tesseract::TessHOcrRenderer(outputbase, font_info);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create hOCR output file: %s\n",
+                strerror(errno));
+      }
     }
 
     api->GetBoolVariable("tessedit_create_tsv", &b);
     if (b) {
       bool font_info;
       api->GetBoolVariable("hocr_font_info", &font_info);
-      renderers->push_back(
-          new tesseract::TessTsvRenderer(outputbase, font_info));
+      tesseract::TessTsvRenderer* renderer =
+          new tesseract::TessTsvRenderer(outputbase, font_info);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create TSV output file: %s\n",
+                strerror(errno));
+      }
     }
 
     api->GetBoolVariable("tessedit_create_pdf", &b);
@@ -427,23 +440,51 @@ static void PreloadRenderers(
       #endif  // WIN32
       bool textonly;
       api->GetBoolVariable("textonly_pdf", &textonly);
-      renderers->push_back(new tesseract::TessPDFRenderer(
-          outputbase, api->GetDatapath(), textonly));
+      tesseract::TessPDFRenderer* renderer =
+        new tesseract::TessPDFRenderer(outputbase, api->GetDatapath(),
+                                       textonly);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create PDF output file: %s\n",
+                strerror(errno));
+      }
     }
 
     api->GetBoolVariable("tessedit_write_unlv", &b);
     if (b) {
-      renderers->push_back(new tesseract::TessUnlvRenderer(outputbase));
+      tesseract::TessUnlvRenderer* renderer =
+        new tesseract::TessUnlvRenderer(outputbase);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create UNLV output file: %s\n",
+                strerror(errno));
+      }
     }
 
     api->GetBoolVariable("tessedit_create_boxfile", &b);
     if (b) {
-      renderers->push_back(new tesseract::TessBoxTextRenderer(outputbase));
+      tesseract::TessBoxTextRenderer* renderer =
+        new tesseract::TessBoxTextRenderer(outputbase);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create BOX output file: %s\n",
+                strerror(errno));
+      }
     }
 
     api->GetBoolVariable("tessedit_create_txt", &b);
     if (b || renderers->empty()) {
-      renderers->push_back(new tesseract::TessTextRenderer(outputbase));
+      tesseract::TessTextRenderer* renderer =
+        new tesseract::TessTextRenderer(outputbase);
+      if (renderer->happy()) {
+        renderers->push_back(renderer);
+      } else {
+        tprintf("Error, could not create TXT output file: %s\n",
+                strerror(errno));
+      }
     }
   }
 

commit be0cf03778bc1c0ed81a5b45a39b15ae40cbcfdd

tesseractmain: Fix memory leak
Commit 49d7df6dc3efc61861c783021e06567d3d4780f0 introduced a memory leak
when the output file could not be created.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/api/tesseractmain.cpp
@@ -413,6 +413,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create hOCR output file: %s\n",
                 strerror(errno));
       }
@@ -427,6 +428,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create TSV output file: %s\n",
                 strerror(errno));
       }
@@ -446,6 +448,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create PDF output file: %s\n",
                 strerror(errno));
       }
@@ -458,6 +461,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create UNLV output file: %s\n",
                 strerror(errno));
       }
@@ -470,6 +474,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create BOX output file: %s\n",
                 strerror(errno));
       }
@@ -482,6 +487,7 @@ static void PreloadRenderers(
       if (renderer->happy()) {
         renderers->push_back(renderer);
       } else {
+        delete renderer;
         tprintf("Error, could not create TXT output file: %s\n",
                 strerror(errno));
       }