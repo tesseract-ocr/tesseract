8b1c858b4e4dd48cecdba583e57d0dd18c899a06
Stefan Weil <sw@weilnetz.de>
2019-01-22 15:22:30 +0100

    unittest: Fix and enable validate_khmer_test
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- unittest/validate_khmer_test.cc
+++ unittest/validate_khmer_test.cc
@@ -1,13 +1,24 @@
-#include "tesseract/training/normstrngs.h"
+// (C) Copyright 2017, Google Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include "tesseract/unittest/normstrngs_test.h"
+#include "include_gunit.h"
+#include "normstrngs.h"
+#include "normstrngs_test.h"
 
 namespace tesseract {
 namespace {
 
 // Test some random Khmer words.
 TEST(ValidateKhmerTest, GoodKhmerWords) {
-  string str = "ព័ត៏មានប្លែកៗ";
+  std::string str = "ព័ត៏មានប្លែកៗ";
   ExpectGraphemeModeResults(str, UnicodeNormMode::kNFC, 13, 12, 7, str);
   str = "ទំនុកច្រៀង";
   ExpectGraphemeModeResults(str, UnicodeNormMode::kNFC, 10, 9, 5, str);
@@ -19,9 +30,9 @@ TEST(ValidateKhmerTest, GoodKhmerWords) {
 
 // Test some random Khmer words with dotted circles.
 TEST(ValidateKhmerTest, BadKhmerWords) {
-  string result;
+  std::string result;
   // Multiple dependent vowels not allowed
-  string str = "\u1796\u17b6\u17b7";
+  std::string str = "\u1796\u17b6\u17b7";
   EXPECT_FALSE(NormalizeUTF8String(UnicodeNormMode::kNFC, OCRNorm::kNone,
                                    GraphemeNorm::kNormalize, str.c_str(),
                                    &result));
