tesseract-ocr/tesseract

commit 32e9d7c8f54c1fa2b4164091396be544b8ba04c8

training: Fix some compiler warnings (signed/unsigned)
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/training/validate_indic.cpp
@@ -95,7 +95,7 @@ Validator::CharClass ValidateIndic::UnicodeToCharClass(char32 ch) const {
 // representation to make it consistent by adding a ZWNJ if missing from a
 // non-linking virama. Returns false with an invalid sequence.
 bool ValidateIndic::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   if (joiner.first == CharClass::kOther) {
     CodeOnlyToOutput();
     if (codes_used_ < num_codes &&
@@ -167,7 +167,7 @@ bool ValidateIndic::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
 // Helper consumes/copies a series of consonants separated by viramas while
 // valid, but not any vowel or other modifiers.
 bool ValidateIndic::ConsumeConsonantHeadIfValid() {
-  const int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   // Consonant aksara
   do {
     CodeOnlyToOutput();

src/training/validate_javanese.cpp
@@ -71,7 +71,7 @@ bool ValidateJavanese::ConsumeGraphemeIfValid() {
 // representation to make it consistent by adding a ZWNJ if missing from a
 // non-linking virama. Returns false with an invalid sequence.
 bool ValidateJavanese::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   if (joiner.first == CharClass::kOther) {
     CodeOnlyToOutput();
     if (codes_used_ < num_codes &&
@@ -143,7 +143,7 @@ bool ValidateJavanese::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
 // Helper consumes/copies a series of consonants separated by viramas while
 // valid, but not any vowel or other modifiers.
 bool ValidateJavanese::ConsumeConsonantHeadIfValid() {
-  const int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   // Consonant aksara
   do {
     CodeOnlyToOutput();
@@ -252,8 +252,8 @@ bool ValidateJavanese::ConsumeVowelIfValid() {
   // What we have consumed so far is a valid vowel cluster.
   return true;
 }
- 
- 
+
+
 Validator::CharClass ValidateJavanese::UnicodeToCharClass(char32 ch) const {
   if (ch == kZeroWidthNonJoiner) return CharClass::kZeroWidthNonJoiner;
   if (ch == kZeroWidthJoiner) return CharClass::kZeroWidthJoiner;

src/training/validate_khmer.cpp
@@ -18,7 +18,7 @@ namespace tesseract {
 // HC and the {Z|z}M The unicode chapter on Khmer only mentions the joiners in
 // the BNF syntax, so who knows what they do.
 bool ValidateKhmer::ConsumeGraphemeIfValid() {
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   if (codes_used_ == num_codes) return false;
   if (codes_[codes_used_].first == CharClass::kOther) {
     UseMultiCode(1);

src/training/validator.h
@@ -235,9 +235,9 @@ class Validator {
   // Copied validated unicodes from codes_ that are OK to output.
   std::vector<char32> output_;
   // The number of elements of codes_ that have been processed so far.
-  int codes_used_;
+  unsigned codes_used_;
   // The number of elements of output_ that have already been added to parts_.
-  int output_used_;
+  unsigned output_used_;
   // Log error messages for reasons why text is invalid.
   bool report_errors_;
 };

src/training/validate_grapheme.cpp
@@ -5,7 +5,7 @@
 namespace tesseract {
 
 bool ValidateGrapheme::ConsumeGraphemeIfValid() {
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   char32 prev_prev_ch = ' ';
   char32 prev_ch = ' ';
   CharClass prev_cc = CharClass::kWhitespace;

src/training/validate_myanmar.cpp
@@ -11,7 +11,7 @@ namespace tesseract {
 // Taken directly from the unicode table 16-3.
 // See http://www.unicode.org/versions/Unicode9.0.0/ch16.pdf
 bool ValidateMyanmar::ConsumeGraphemeIfValid() {
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   if (codes_used_ == num_codes) return true;
   // Other.
   if (IsMyanmarOther(codes_[codes_used_].second)) {
@@ -61,7 +61,7 @@ Validator::CharClass ValidateMyanmar::UnicodeToCharClass(char32 ch) const {
 // Returns true if the end of input is reached.
 bool ValidateMyanmar::ConsumeSubscriptIfPresent() {
   // Subscript consonant. It appears there can be only one.
-  int num_codes = codes_.size();
+  const unsigned num_codes = codes_.size();
   if (codes_used_ + 1 < num_codes &&
       codes_[codes_used_].second == kMyanmarVirama) {
     if (IsMyanmarLetter(codes_[codes_used_ + 1].second)) {

commit 0e1a1fc3cf6909ec10628923a90f4bdebb347aaf

Validator: Fix compiler warnings (signed/unsigned)
This also fixes a regression in validate_grapheme_test introduced
by commit 32e9d7c8f54c1fa2b4164091396be544b8ba04c8.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/training/validate_indic.cpp
@@ -120,7 +120,7 @@ bool ValidateIndic::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
         ASSERT_HOST(!CodeOnlyToOutput());
       } else {
         // Half-form with optional Nukta.
-        int len = output_.size() + 1 - output_used_;
+        unsigned len = output_.size() + 1 - output_used_;
         if (UseMultiCode(len)) return true;
       }
       if (codes_used_ < num_codes &&
@@ -179,7 +179,7 @@ bool ValidateIndic::ConsumeConsonantHeadIfValid() {
     CodeOnlyToOutput();
     // Special Sinhala case of [H Z Yayana/Rayana].
     int index = output_.size() - 3;
-    if (output_used_ <= index &&
+    if (output_used_ + 3 <= output_.size() &&
         (output_.back() == kYayana || output_.back() == kRayana) &&
         IsVirama(output_[index]) && output_[index + 1] == kZeroWidthJoiner) {
       MultiCodePart(3);
@@ -192,7 +192,7 @@ bool ValidateIndic::ConsumeConsonantHeadIfValid() {
     }
     // Test for subscript conjunct.
     index = output_.size() - 2 - have_nukta;
-    if (output_used_ <= index && IsSubscriptScript() &&
+    if (output_used_ + 2 + have_nukta <= output_.size() && IsSubscriptScript() &&
         IsVirama(output_[index])) {
       // Output previous virama, consonant + optional nukta.
       MultiCodePart(2 + have_nukta);

src/training/validate_javanese.cpp
@@ -2,7 +2,6 @@
  * File:        validate_javanese.cpp
  * Description: Text validator for Javanese Script - aksara jawa.
  * Author:      Shree Devi Kumar
- * Created:     August 03, 2018
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +89,7 @@ bool ValidateJavanese::ConsumeViramaIfValid(IndicPair joiner, bool post_matra) {
         ASSERT_HOST(!CodeOnlyToOutput());
       } else {
         // Half-form with optional Nukta.
-        int len = output_.size() + 1 - output_used_;
+        unsigned len = output_.size() + 1 - output_used_;
         if (UseMultiCode(len)) return true;
       }
       if (codes_used_ < num_codes &&
@@ -149,7 +148,7 @@ bool ValidateJavanese::ConsumeConsonantHeadIfValid() {
     CodeOnlyToOutput();
     // Special Sinhala case of [H Z Yayana/Rayana].
     int index = output_.size() - 3;
-    if (output_used_ <= index &&
+    if (output_used_ + 3 <= output_.size() &&
         (output_.back() == kPengkal || output_.back() == kCakra) &&
         IsVirama(output_[index]) && output_[index + 1] == kZeroWidthJoiner) {
       MultiCodePart(3);
@@ -162,7 +161,7 @@ bool ValidateJavanese::ConsumeConsonantHeadIfValid() {
     }
     // Test for subscript conjunct.
     index = output_.size() - 2 - have_nukta;
-    if (output_used_ <= index && IsSubscriptScript() &&
+    if (output_used_ + 2 + have_nukta <= output_.size() && IsSubscriptScript() &&
         IsVirama(output_[index])) {
       // Output previous virama, consonant + optional nukta.
       MultiCodePart(2 + have_nukta);

src/training/validate_khmer.cpp
@@ -45,7 +45,7 @@ bool ValidateKhmer::ConsumeGraphemeIfValid() {
       if (UseMultiCode(1)) return true;
     }
   }
-  int num_matra_parts = 0;
+  unsigned num_matra_parts = 0;
   if (codes_[codes_used_].second == kZeroWidthJoiner ||
       codes_[codes_used_].second == kZeroWidthNonJoiner) {
     if (CodeOnlyToOutput()) {

src/training/validator.h
@@ -3,7 +3,6 @@
  * Description: Base class for various text validators. Intended mainly for
  *              scripts that use a virama character.
  * Author:      Ray Smith
- * Created:     Tue May 23 2017
  *
  * (C) Copyright 2017, Google Inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -179,7 +178,7 @@ class Validator {
   // output_, adds unicodes as single-element vectors to parts_ to catch
   // output_used_ up to output->size() - length before adding the length-element
   // vector.
-  void MultiCodePart(int length) {
+  void MultiCodePart(unsigned length) {
     while (output_used_ + length < output_.size()) {
       parts_.emplace_back(
           std::initializer_list<char32>{output_[output_used_++]});
@@ -193,7 +192,7 @@ class Validator {
   // Helper function appends the next element of codes_ to output_, and then
   // calls MultiCodePart to add the appropriate components to parts_.
   // Returns true at the end of codes_.
-  bool UseMultiCode(int length) {
+  bool UseMultiCode(unsigned length) {
     output_.push_back(codes_[codes_used_].second);
     MultiCodePart(length);
     return ++codes_used_ == codes_.size();