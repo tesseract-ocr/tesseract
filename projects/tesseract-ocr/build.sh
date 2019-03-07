1a7f633ab054c944c0d4b16d00253f474571d863
Guido Vranken <guidovranken@users.noreply.github.com>
2019-03-07 15:57:21 +0100

    Add Tesseract (#2210)
    * Add Tesseract
    
    * Use -lz instead of static library path
    
    * Disable Tesseract shared build
    
    * Minimal repository cloning (--depth 1)
    
    * Improve tessdata directory resolution syntax
    
    * Don't hardcode TESSDATA_PREFIX into binary
    
    * Don't move, but copy $SRC/tessdata to $OUT
    
    Move sometimes results in "inter-device move failed"

---
+++ projects/tesseract-ocr/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -eu
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd $SRC
+tar zxf leptonica-1.77.0.tar.gz
+cd leptonica-1.77.0
+./configure
+make -j$(nproc)
+make install
+ldconfig
+
+cd $SRC/tesseract
+./autogen.sh
+CXXFLAGS="$CXXFLAGS -D_GLIBCXX_DEBUG" ./configure --disable-graphics --disable-shared
+make -j$(nproc)
+
+cd $SRC/tesseract-ocr-fuzzers
+
+cp -R $SRC/tessdata $OUT
+
+$CXX $CXXFLAGS \
+    -I $SRC/tesseract/src/api \
+    -I $SRC/tesseract/src/ccstruct \
+    -I $SRC/tesseract/src/ccmain \
+    -I $SRC/tesseract/src/ccutil \
+     $SRC/tesseract-ocr-fuzzers/fuzzer-api.cpp -o $OUT/fuzzer-api \
+     $SRC/tesseract/src/api/.libs/libtesseract.a \
+     /usr/local/lib/liblept.a \
+     /usr/lib/x86_64-linux-gnu/libtiff.a \
+     /usr/lib/x86_64-linux-gnu/libpng.a \
+     /usr/lib/x86_64-linux-gnu/libjpeg.a \
+     /usr/lib/x86_64-linux-gnu/libjbig.a \
+     /usr/lib/x86_64-linux-gnu/liblzma.a \
+     -lz \
+     -lFuzzingEngine
