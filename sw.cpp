37b0c36e32b4b58357af225ef0e74260813a8745
Egor Pugin <egor.pugin@gmail.com>
2019-03-11 00:03:45 +0300

    Add libarchive dependency to cppan and sw builds.

--- sw.cpp
+++ sw.cpp
@@ -71,10 +71,12 @@ void build(Solution &s)
         libtesseract.Public += "HAVE_CONFIG_H"_d;
         libtesseract.Public += "WINDLLNAME=\"tesseract\""_d;
         libtesseract.Public += "_SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS=1"_d;
+        libtesseract.Public += "HAVE_LIBARCHIVE"_d;
         libtesseract.Interface += sw::Shared, "TESS_IMPORTS"_d;
         libtesseract.Private += sw::Shared, "TESS_EXPORTS"_d;
 
         libtesseract.Public += "org.sw.demo.danbloomberg.leptonica-master"_dep;
+        libtesseract.Public += "org.sw.demo.libarchive.libarchive"_dep;
 
         if (s.Settings.TargetOS.Type == OSType::Windows)
             libtesseract.Public += "ws2_32.lib"_l;
