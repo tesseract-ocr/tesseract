da0c14ae4573b3536de3741bb426178aec59b369
Stefan Weil <sw@weilnetz.de>
2019-03-30 17:06:58 +0100

    cutil: Replace TRUE, FALSE by true, false
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/cutil/oldlist.cpp
+++ src/cutil/oldlist.cpp
@@ -85,8 +85,8 @@
 /**********************************************************************
  *  i s   s a m e
  *
- *  Compare the list node with the key value return TRUE (non-zero)
- *  if they are equivalent strings.  (Return FALSE if not)
+ *  Compare the list node with the key value return true (non-zero)
+ *  if they are equivalent strings.  (Return false if not)
  **********************************************************************/
 static int is_same(void *item1, void *item2) {
   return strcmp((char *)item1, (char *)item2) == 0;
