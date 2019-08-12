bce585286d2a1bc24e8bdb28ac34b60c37fe07d6
Stefan Weil <sw@weilnetz.de>
2019-08-12 14:33:55 +0200

    Remove global array kPolyBlockNames from Tesseract library
    It is only used in unittest/layout_test.cc after moving a test from
    baseapi_test.cc to that file, so it can be made local.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- unittest/layout_test.cc
+++ unittest/layout_test.cc
@@ -31,6 +31,26 @@ using tesseract::MutableIterator;
 using tesseract::PageIteratorLevel;
 using tesseract::ResultIterator;
 
+/** String name for each block type. Keep in sync with PolyBlockType. */
+static const char* kPolyBlockNames[] = {
+  "Unknown",
+  "Flowing Text",
+  "Heading Text",
+  "Pullout Text",
+  "Equation",
+  "Inline Equation",
+  "Table",
+  "Vertical Text",
+  "Caption Text",
+  "Flowing Image",
+  "Heading Image",
+  "Pullout Image",
+  "Horizontal Line",
+  "Vertical Line",
+  "Noise",
+  ""  // End marker for testing that sizes match.
+};
+
 const char* kStrings8087_054[] = {
     "dat", "Dalmatian", "", "DAMAGED DURING", "margarine,", nullptr};
 const PolyBlockType kBlocks8087_054[] = {PT_HEADING_TEXT, PT_FLOWING_TEXT,
@@ -170,6 +190,14 @@ class LayoutTest : public testing::Test {
   std::string ocr_text_;
   tesseract::TessBaseAPI api_;
 };
+
+// Tests that array sizes match their intended size.
+TEST_F(LayoutTest, ArraySizeTest) {
+  int size = 0;
+  for (size = 0; kPolyBlockNames[size][0] != '\0'; ++size)
+    ;
+  EXPECT_EQ(size, PT_COUNT);
+}
 
 // Tests that Tesseract gets the important blocks and in the right order
 // on a UNLV page numbered 8087_054.3B.tif. (Dubrovnik)
