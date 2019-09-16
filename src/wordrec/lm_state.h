tesseract-ocr/tesseract

commit 091ce345f68e549f7683c816f6c1afef7e54fd73

Optimize class ViterbiStateEntry
Reduce size from 232 to 216 bytes by avoiding holes.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/wordrec/lm_state.h
@@ -2,9 +2,7 @@
 // File:        lm_state.h
 // Description: Structures and functionality for capturing the state of
 //              segmentation search guided by the language model.
-//
 // Author:      Rika Antonova
-// Created:     Mon Jun 20 11:26:43 PST 2012
 //
 // (C) Copyright 2012, Google Inc.
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -150,16 +148,28 @@ struct ViterbiStateEntry : public ELIST_LINK {
   }
   void Print(const char *msg) const;
 
-  /// The cost is an adjusted ratings sum, that is adjusted by all the language
-  /// model components that use Viterbi search.
-  float cost;
-
   /// Pointers to BLOB_CHOICE and parent ViterbiStateEntry (not owned by this).
   BLOB_CHOICE *curr_b;
   ViterbiStateEntry *parent_vse;
   /// Pointer to a case-competing ViterbiStateEntry in the same list that
   /// represents a path ending in the same letter of the opposite case.
   ViterbiStateEntry *competing_vse;
+
+  /// Extra information maintained by Dawg language model component
+  /// (owned by ViterbiStateEntry).
+  LanguageModelDawgInfo *dawg_info;
+
+  /// Extra information maintained by Ngram language model component
+  /// (owned by ViterbiStateEntry).
+  LanguageModelNgramInfo *ngram_info;
+
+  /// UTF8 string representing the path corresponding to this vse.
+  /// Populated only in when language_model_debug_level > 0.
+  STRING *debug_str;
+
+  /// The cost is an adjusted ratings sum, that is adjusted by all the language
+  /// model components that use Viterbi search.
+  float cost;
 
   /// Various information about the characters on the path represented
   /// by this ViterbiStateEntry.
@@ -175,18 +185,7 @@ struct ViterbiStateEntry : public ELIST_LINK {
   /// the smallest rating or lower/upper case letters).
   LanguageModelFlagsType top_choice_flags;
 
-  /// Extra information maintained by Dawg language model component
-  /// (owned by ViterbiStateEntry).
-  LanguageModelDawgInfo *dawg_info;
-
-  /// Extra information maintained by Ngram language model component
-  /// (owned by ViterbiStateEntry).
-  LanguageModelNgramInfo *ngram_info;
-
   bool updated;  ///< set to true if the entry has just been created/updated
-  /// UTF8 string representing the path corresponding to this vse.
-  /// Populated only in when language_model_debug_level > 0.
-  STRING *debug_str;
 };
 
 ELISTIZEH(ViterbiStateEntry)

commit 256701e2e088810629f28cd6f572cb715661c545

Re-order initialisation in constructor of class ViterbiStateEntry
This fixes compiler warnings caused by
commit 091ce345f68e549f7683c816f6c1afef7e54fd73:

    src/wordrec/lm_state.h:100:7: warning: field 'cost'
      will be initialized after field 'curr_b' [-Wreorder]
    src/wordrec/lm_state.h:104:7: warning: field 'top_choice_flags'
      will be initialized after field 'dawg_info' [-Wreorder]

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/wordrec/lm_state.h
@@ -97,11 +97,17 @@ struct ViterbiStateEntry : public ELIST_LINK {
                     LanguageModelDawgInfo *d,
                     LanguageModelNgramInfo *n,
                     const char *debug_uch)
-    : cost(c), curr_b(b), parent_vse(pe), competing_vse(nullptr),
+    : curr_b(b), parent_vse(pe), competing_vse(nullptr),
+      dawg_info(d), ngram_info(n),
+      cost(c),
       ratings_sum(b->rating()),
-      min_certainty(b->certainty()), adapted(b->IsAdapted()), length(1),
-      outline_length(ol), consistency_info(ci), associate_stats(as),
-      top_choice_flags(tcf), dawg_info(d), ngram_info(n),
+      min_certainty(b->certainty()),
+      adapted(b->IsAdapted()),
+      length(1),
+      outline_length(ol),
+      consistency_info(ci),
+      associate_stats(as),
+      top_choice_flags(tcf),
       updated(true) {
     debug_str = (debug_uch == nullptr) ? nullptr : new STRING();
     if (pe != nullptr) {