tesseract-ocr/tesseract

commit ecf0f2dee5bb7b5e6a673c8449e625502b526b25

Optimize classes Trie, Dawg and DawgPosition
Reduce size from 368 to 352 bytes for Trie, 72 to 64 bytes for Dawg
and 40 to 24 bytes for DawgPosition by avoiding holes.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/dict/dawg.h
@@ -294,20 +294,20 @@ class Dawg {
                          std::function<void(const WERD_CHOICE*)> cb) const;
 
   // Member Variables.
-  DawgType type_;
   STRING lang_;
+  DawgType type_;
   /// Permuter code that should be used if the word is found in this Dawg.
   PermuterType perm_;
   // Variables to construct various edge masks. Formerly:
   // #define NEXT_EDGE_MASK (int64_t) 0xfffffff800000000i64
   // #define FLAGS_MASK     (int64_t) 0x0000000700000000i64
   // #define LETTER_MASK    (int64_t) 0x00000000ffffffffi64
-  int unicharset_size_;
-  int flag_start_bit_;
-  int next_node_start_bit_;
   uint64_t next_node_mask_;
   uint64_t flags_mask_;
   uint64_t letter_mask_;
+  int unicharset_size_;
+  int flag_start_bit_;
+  int next_node_start_bit_;
   // Level of debug statements to print to stdout.
   int debug_level_;
 };
@@ -366,10 +366,10 @@ struct DawgPosition {
         back_to_punc == other.back_to_punc;
   }
 
-  int8_t dawg_index;
   EDGE_REF dawg_ref;
-  int8_t punc_index;
   EDGE_REF punc_ref;
+  int8_t dawg_index;
+  int8_t punc_index;
   // Have we returned to the punc dawg at the end of the word?
   bool back_to_punc;
 };

src/dict/trie.h
@@ -411,20 +411,20 @@ class Trie : public Dawg {
 
   // Member variables
   TRIE_NODES nodes_;              // vector of nodes in the Trie
+  // Freelist of edges in the root backwards node that were previously zeroed.
+  GenericVector<EDGE_INDEX> root_back_freelist_;
   uint64_t num_edges_;              // sum of all edges (forward and backward)
   uint64_t deref_direction_mask_;   // mask for EDGE_REF to extract direction
   uint64_t deref_node_index_mask_;  // mask for EDGE_REF to extract node index
-  // Freelist of edges in the root backwards node that were previously zeroed.
-  GenericVector<EDGE_INDEX> root_back_freelist_;
   // Variables for translating character class codes denoted in user patterns
   // file to the unichar ids used to represent them in a Trie.
-  bool initialized_patterns_;
   UNICHAR_ID alpha_pattern_;
   UNICHAR_ID digit_pattern_;
   UNICHAR_ID alphanum_pattern_;
   UNICHAR_ID punc_pattern_;
   UNICHAR_ID lower_pattern_;
   UNICHAR_ID upper_pattern_;
+  bool initialized_patterns_;
 };
 }  // namespace tesseract
 

commit 8f66020821154c0870bfc890296b96f4927a9ec8

Re-order initialisation in constructors of classes Dawg and DawgPosition
This fixes compiler warnings caused by
commit ecf0f2dee5bb7b5e6a673c8449e625502b526b25:

    src/dict/dawg.h:202:9: warning: field 'type_' will be initialized
      after field 'lang_' [-Wreorder]
    src/dict/dawg.h:355:9: warning: field 'dawg_index' will be initialized
      after field 'dawg_ref' [-Wreorder]
    src/dict/dawg.h:356:9: warning: field 'punc_index' will be initialized
      after field 'punc_ref' [-Wreorder]

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/dict/dawg.h
@@ -199,8 +199,8 @@ class Dawg {
 
  protected:
   Dawg(DawgType type, const STRING &lang, PermuterType perm, int debug_level)
-      : type_(type),
-        lang_(lang),
+      : lang_(lang),
+        type_(type),
         perm_(perm),
         unicharset_size_(0),
         debug_level_(debug_level) {}
@@ -352,8 +352,8 @@ struct DawgPosition {
   DawgPosition(int dawg_idx, EDGE_REF dawgref,
                int punc_idx, EDGE_REF puncref,
                bool backtopunc)
-      : dawg_index(dawg_idx), dawg_ref(dawgref),
-        punc_index(punc_idx), punc_ref(puncref),
+      : dawg_ref(dawgref), punc_ref(puncref),
+        dawg_index(dawg_idx), punc_index(punc_idx),
         back_to_punc(backtopunc) {
   }
   bool operator==(const DawgPosition &other) {