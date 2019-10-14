03a0586931e5e6c8e91719ea4007059e462854c0
zdenop <zdenop@gmail.com>
2019-10-14 22:55:47 +0200

    Report when tesseract legacy engine not present. (fix issue #2053)

--- src/ccmain/tessedit.cpp
+++ src/ccmain/tessedit.cpp
@@ -195,6 +195,8 @@ bool Tesseract::init_tesseract_lang_data(
 #ifndef DISABLED_LEGACY_ENGINE
   else if (!mgr->GetComponent(TESSDATA_UNICHARSET, &fp) ||
            !unicharset.load_from_file(&fp, false)) {
+    tprintf("Error: Tesseract (legacy) engine requested, but components are "
+            "not present in %s!!\n", tessdata_path.c_str());
     return false;
   }
 #endif  // ndef DISABLED_LEGACY_ENGINE
