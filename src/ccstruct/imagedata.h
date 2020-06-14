d1dd3c5522b99aa0376556d31115695739634cdb
Stefan Weil <sw@weilnetz.de>
2020-06-14 14:06:30 +0200

    Fix crash caused by missing thread synchronization (issues #757, #1168 and #2191)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/ccstruct/imagedata.h
+++ src/ccstruct/imagedata.h
@@ -20,9 +20,10 @@
 #define TESSERACT_IMAGE_IMAGEDATA_H_
 
 #include <mutex>                // for std::mutex
+#include <thread>               // for std::thread
 #include <tesseract/genericvector.h>      // for GenericVector, PointerVector, FileReader
 #include "points.h"             // for FCOORD
-#include <tesseract/strngs.h>             // for STRING
+#include <tesseract/strngs.h>   // for STRING
 
 class ScrollView;
 class TBOX;
@@ -313,6 +314,9 @@ class DocumentData {
   // Mutex that protects other data members that callers want to access without
   // waiting for a load operation.
   mutable std::mutex general_mutex_;
+
+  // Thread which loads document.
+  std::thread thread;
 };
 
 // A collection of DocumentData that knows roughly how much memory it is using.
