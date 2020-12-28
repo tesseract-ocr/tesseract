f51a945e264ea9d9dc714a2ddec8b5c31bb8a2db
Egor Pugin <egor.pugin@gmail.com>
2020-12-28 04:31:00 +0300

    Make symbol visible to fix unit test.

--- include/tesseract/baseapi.h
+++ include/tesseract/baseapi.h
@@ -776,7 +776,7 @@ class TESS_API TessBaseAPI {
    * Run the thresholder to make the thresholded image. If pix is not nullptr,
    * the source is thresholded to pix instead of the internal IMAGE.
    */
-  TESS_LOCAL virtual bool Threshold(Pix** pix);
+  TESS_API virtual bool Threshold(Pix** pix);
 
   /**
    * Find lines from the image making the BLOCK_LIST.
