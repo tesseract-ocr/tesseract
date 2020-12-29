deec8ef46fbf2feefb792b4e8eaeff8f63d487ad
Stefan Weil <sw@weil.de>
2020-12-29 22:55:03 +0100

    Replace std::list by std::vector
    Signed-off-by: Stefan Weil <sw@weil.de>

--- src/ccmain/paragraphs.h
+++ src/ccmain/paragraphs.h
@@ -91,7 +91,7 @@ void DetectParagraphs(int debug_level,
                       GenericVector<RowInfo> *row_infos,
                       GenericVector<PARA *> *row_owners,
                       PARA_LIST *paragraphs,
-                      std::list<ParagraphModel *> *models);
+                      std::vector<ParagraphModel *> *models);
 
 // Given a MutableIterator to the start of a block, run DetectParagraphs on
 // that block and commit the results to the underlying ROW and BLOCK structs,
@@ -101,7 +101,7 @@ void DetectParagraphs(int debug_level,
 void DetectParagraphs(int debug_level,
                       bool after_text_recognition,
                       const MutableIterator *block_start,
-                      std::list<ParagraphModel *> *models);
+                      std::vector<ParagraphModel *> *models);
 
 }  // namespace
 
