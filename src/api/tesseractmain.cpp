422452b9f4aa6a669b62c79ccce6374c0d131031
Stefan Weil <sw@weilnetz.de>
2021-02-25 21:26:48 +0100

    Check for float errors when running tesseract and lstmtraining
    Some illegal floating point calculations like division by zero,
    illegal value or overflow will now abort tesseract with an error
    message.
    
    For lstmtraining there is now a new parameter --debug_float to
    enable the same kind of checks. It is currently disabled by default
    because such errors occur and would abort the training process.
    That should be fixed in the future.
    
    If tesseract also shows floating point errors which cannot be
    fixed easily, a similar parameter to enable the checks can be
    added there, too.
    
    The new code requires the function feenableexcept which is only
    available with the GNU libc, so it is only used on Linux.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/api/tesseractmain.cpp
+++ src/api/tesseractmain.cpp
@@ -22,6 +22,9 @@
 #endif
 
 #include <cerrno>               // for errno
+#if defined(__USE_GNU)
+#include <cfenv>                // for feenableexcept
+#endif
 #include <iostream>
 
 #include <allheaders.h>
@@ -617,6 +620,10 @@ static void PreloadRenderers(
  **********************************************************************/
 
 int main(int argc, char** argv) {
+#if defined(__USE_GNU)
+  // Raise SIGFPE.
+  feenableexcept(FE_DIVBYZERO | FE_OVERFLOW | FE_INVALID);
+#endif
   const char* lang = nullptr;
   const char* image = nullptr;
   const char* outputbase = nullptr;
