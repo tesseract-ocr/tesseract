tesseract-ocr/tesseract

commit f33e80e2fbda202b524008995fdbc2bd64afec88

Replace remaining PointerVector by std::vector for src/lstm
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/textord/tordmain.cpp
@@ -49,8 +49,6 @@
 #include "tprintf.h"     // for tprintf
 #include "werd.h"        // for WERD_IT, WERD, WERD_LIST, W_DONT_CHOP
 
-#include "genericvector.h" // for PointerVector
-
 #include <allheaders.h> // for pixDestroy, pixGetHeight, boxCreate
 
 #include <cfloat>  // for FLT_MAX
@@ -726,9 +724,9 @@ void Textord::TransferDiacriticsToBlockGroups(BLOBNBOX_LIST *diacritic_blobs, BL
     }
   }
   // Now process each group of blocks.
-  PointerVector<WordWithBox> word_ptrs;
-  for (int g = 0; g < groups.size(); ++g) {
-    const BlockGroup *group = groups[g];
+  std::vector<WordWithBox *> word_ptrs;
+  word_ptrs.reserve(groups.size());
+  for (const auto group : groups) {
     if (group->bounding_box.null_box())
       continue;
     WordGrid word_grid(group->min_xheight, group->bounding_box.botleft(),
@@ -747,6 +745,9 @@ void Textord::TransferDiacriticsToBlockGroups(BLOBNBOX_LIST *diacritic_blobs, BL
           word_ptrs.push_back(box_word);
         }
       }
+    }
+    for (auto box_word : word_ptrs) {
+      delete box_word;
     }
     FCOORD rotation = group->rotation;
     // Make it a forward rotation that will transform blob coords to block.

src/lstm/networkscratch.h
@@ -20,7 +20,6 @@
 #define TESSERACT_LSTM_NETWORKSCRATCH_H_
 
 #include <mutex>
-#include "genericvector.h" // for PointerVector
 #include "matrix.h"
 #include "networkio.h"
 
@@ -217,6 +216,12 @@ public:
   public:
     Stack() : stack_top_(0) {}
 
+    ~Stack() {
+      for (auto data : stack_) {
+        delete data;
+      }
+    }
+
     // Lends out the next free item, creating one if none available, sets
     // the used flags and increments the stack top.
     T *Borrow() {
@@ -246,7 +251,7 @@ public:
     }
 
   private:
-    PointerVector<T> stack_;
+    std::vector<T *> stack_;
     std::vector<bool> flags_;
     int stack_top_;
     std::mutex mutex_;

src/lstm/plumbing.cpp
@@ -198,7 +198,10 @@ bool Plumbing::Serialize(TFile *fp) const {
 
 // Reads from the given file. Returns false in case of error.
 bool Plumbing::DeSerialize(TFile *fp) {
-  stack_.truncate(0);
+  for (auto data : stack_) {
+    delete data;
+  }
+  stack_.clear();
   no_ = 0; // We will be modifying this as we AddToStack.
   uint32_t size;
   if (!fp->DeSerialize(&size))

src/lstm/plumbing.h
@@ -19,7 +19,6 @@
 #ifndef TESSERACT_LSTM_PLUMBING_H_
 #define TESSERACT_LSTM_PLUMBING_H_
 
-#include "genericvector.h" // for PointerVector
 #include "matrix.h"
 #include "network.h"
 
@@ -30,7 +29,11 @@ class Plumbing : public Network {
 public:
   // ni_ and no_ will be set by AddToStack.
   explicit Plumbing(const std::string &name);
-  ~Plumbing() override = default;
+  ~Plumbing() override {
+    for (auto data : stack_) {
+      delete data;
+    }
+  }
 
   // Returns the required shape input to the network.
   StaticShape InputShape() const override {
@@ -96,7 +99,7 @@ public:
   void DebugWeights() override;
 
   // Returns the current stack.
-  const PointerVector<Network> &stack() const {
+  const std::vector<Network *> &stack() const {
     return stack_;
   }
   // Returns a set of strings representing the layer-ids of all layers below.
@@ -136,7 +139,7 @@ public:
 
 protected:
   // The networks.
-  PointerVector<Network> stack_;
+  std::vector<Network *> stack_;
   // Layer-specific learning rate iff network_flags_ & NF_LAYER_SPECIFIC_LR.
   // One element for each element of stack_.
   std::vector<float> learning_rates_;

src/textord/baselinedetect.h
@@ -20,7 +20,6 @@
 #define TESSERACT_TEXTORD_BASELINEDETECT_H_
 
 #include "detlinefit.h"
-#include "genericvector.h" // for PointerVector
 #include "points.h"
 #include "rect.h"
 
@@ -127,6 +126,12 @@ class BaselineBlock {
 public:
   BaselineBlock(int debug_level, bool non_text, TO_BLOCK *block);
 
+  ~BaselineBlock() {
+    for (auto row : rows_) {
+      delete row;
+    }
+  }
+
   TO_BLOCK *block() const {
     return block_;
   }
@@ -209,7 +214,7 @@ private:
   // calculation.
   TO_BLOCK *block_;
   // The rows in the block that we will be working with.
-  PointerVector<BaselineRow> rows_;
+  std::vector<BaselineRow *> rows_;
   // Amount of debugging output to provide.
   int debug_level_;
   // True if the block is non-text (graphic).
@@ -235,7 +240,11 @@ class BaselineDetect {
 public:
   BaselineDetect(int debug_level, const FCOORD &page_skew, TO_BLOCK_LIST *blocks);
 
-  ~BaselineDetect() = default;
+  ~BaselineDetect() {
+    for (auto block : blocks_) {
+      delete block;
+    }
+  }
 
   // Finds the initial baselines for each TO_ROW in each TO_BLOCK, gathers
   // block-wise and page-wise data to smooth small blocks/rows, and applies
@@ -257,7 +266,7 @@ private:
   // Amount of debug output to produce.
   int debug_level_;
   // The blocks that we are working with.
-  PointerVector<BaselineBlock> blocks_;
+  std::vector<BaselineBlock *> blocks_;
 };
 
 } // namespace tesseract

commit f176e7c274f9b208daca7b074380417ad4e78c05

Fix double free caused by commit f33e80e (fixes issue #3348)
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/textord/tordmain.cpp
@@ -725,7 +725,6 @@ void Textord::TransferDiacriticsToBlockGroups(BLOBNBOX_LIST *diacritic_blobs, BL
   }
   // Now process each group of blocks.
   std::vector<WordWithBox *> word_ptrs;
-  word_ptrs.reserve(groups.size());
   for (const auto group : groups) {
     if (group->bounding_box.null_box())
       continue;
@@ -746,13 +745,13 @@ void Textord::TransferDiacriticsToBlockGroups(BLOBNBOX_LIST *diacritic_blobs, BL
         }
       }
     }
-    for (auto box_word : word_ptrs) {
-      delete box_word;
-    }
     FCOORD rotation = group->rotation;
     // Make it a forward rotation that will transform blob coords to block.
     rotation.set_y(-rotation.y());
     TransferDiacriticsToWords(diacritic_blobs, rotation, &word_grid);
+  }
+  for (auto box_word : word_ptrs) {
+    delete box_word;
   }
   for (auto group : groups) {
     delete group;