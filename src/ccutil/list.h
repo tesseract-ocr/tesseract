2e17ee73270c90f93f00217b7825af2e837819da
Egor Pugin <egor.pugin@gmail.com>
2021-04-07 13:28:57 +0300

    Correct template args.

--- src/ccutil/list.h
+++ src/ccutil/list.h
@@ -18,28 +18,28 @@
 
 namespace tesseract {
 
-template <typename CONTAINER, typename CLASSNAME>
-class X_ITER : public CONTAINER {
+template <typename ITERATOR, typename CLASSNAME>
+class X_ITER : public ITERATOR {
 public:
   X_ITER() = default;
   template <typename U>
-  X_ITER(U *list) : CONTAINER(list) {}
+  X_ITER(U *list) : ITERATOR(list) {}
 
   CLASSNAME *data() {
-    return static_cast<CLASSNAME *>(CONTAINER::data());
+    return static_cast<CLASSNAME *>(ITERATOR::data());
   }
   CLASSNAME *data_relative(int8_t offset) {
-    return static_cast<CLASSNAME *>(CONTAINER::data_relative(offset));
+    return static_cast<CLASSNAME *>(ITERATOR::data_relative(offset));
   }
   CLASSNAME *forward() {
-    return static_cast<CLASSNAME *>(CONTAINER::forward());
+    return static_cast<CLASSNAME *>(ITERATOR::forward());
   }
   CLASSNAME *extract() {
-    return static_cast<CLASSNAME *>(CONTAINER::extract());
+    return static_cast<CLASSNAME *>(ITERATOR::extract());
   }
 };
 
-template <typename CONTAINER, typename ITERATOR_TYPE, typename CLASSNAME>
+template <typename CONTAINER, typename ITERATOR, typename CLASSNAME>
 class X_LIST : public CONTAINER {
 public:
   X_LIST() = default;
@@ -57,8 +57,8 @@ public:
   /* Become a deep copy of src_list */
   template <typename U>
   void deep_copy(const U *src_list, CLASSNAME *(*copier)(const CLASSNAME *)) {
-    X_ITER<ITERATOR_TYPE, CLASSNAME> from_it(const_cast<U *>(src_list));
-    X_ITER<ITERATOR_TYPE, CLASSNAME> to_it(this);
+    X_ITER<ITERATOR, CLASSNAME> from_it(const_cast<U *>(src_list));
+    X_ITER<ITERATOR, CLASSNAME> to_it(this);
 
     for (from_it.mark_cycle_pt(); !from_it.cycled_list(); from_it.forward())
       to_it.add_after_then_move((*copier)(from_it.data()));
