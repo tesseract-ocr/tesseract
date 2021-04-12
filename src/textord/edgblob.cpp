tesseract-ocr/tesseract

commit 57c90eee02866ec929d8cd42e2de73a04464479f

[edgblob] Replace unique ptr with vector. Fix possible index issues.
Closes #1921.

src/textord/edgblob.cpp
@@ -23,10 +23,12 @@
 
 #include "edgblob.h"
 
-#include <memory>
-
 #include "edgloop.h"
 #include "scanedg.h"
+
+#include <memory>
+
+#define BUCKETSIZE 16
 
 namespace tesseract {
 
@@ -58,12 +60,11 @@ static double_VAR(edges_boxarea, 0.875, "Min area fraction of grandchild for box
 
 OL_BUCKETS::OL_BUCKETS(ICOORD bleft, // corners
                        ICOORD tright)
-    : bl(bleft), tr(tright) {
-  bxdim = (tright.x() - bleft.x()) / BUCKETSIZE + 1;
-  bydim = (tright.y() - bleft.y()) / BUCKETSIZE + 1;
-  // make array
-  buckets = std::make_unique<C_OUTLINE_LIST[]>(bxdim * bydim);
-  index = 0;
+    : bxdim((tright.x() - bleft.x()) / BUCKETSIZE + 1),
+      bydim((tright.y() - bleft.y()) / BUCKETSIZE + 1),
+      buckets(bxdim * bydim),
+      bl(bleft),
+      tr(tright) {
 }
 
 /**
@@ -77,6 +78,21 @@ C_OUTLINE_LIST *OL_BUCKETS::operator()( // array access
     int16_t x,                          // image coords
     int16_t y) {
   return &buckets[(y - bl.y()) / BUCKETSIZE * bxdim + (x - bl.x()) / BUCKETSIZE];
+}
+
+C_OUTLINE_LIST *OL_BUCKETS::start_scan() {
+  return scan_next(buckets.begin());
+}
+
+C_OUTLINE_LIST *OL_BUCKETS::scan_next() {
+  return scan_next(it);
+}
+
+C_OUTLINE_LIST *OL_BUCKETS::scan_next(decltype(buckets)::iterator in_it) {
+  it = std::find_if(in_it, buckets.end(), [](auto &&b) { return !b.empty(); });
+  if (it == buckets.end())
+    return nullptr;
+  return &*it;
 }
 
 /**
@@ -408,7 +424,10 @@ void empty_buckets(     // find blobs
     good_blob = capture_children(buckets, &junk_blobs, &out_it);
     C_BLOB::ConstructBlobsFromOutlines(good_blob, &outlines, &good_blobs, &junk_blobs);
 
-    bucket_it.set_to_list(buckets->scan_next());
+    if (auto l = buckets->scan_next())
+      bucket_it.set_to_list(l);
+    else
+      break;
   }
 }
 

src/textord/edgblob.h
@@ -26,10 +26,9 @@
 #include "scrollview.h"
 
 #include <memory>
+#include <vector>
 
 namespace tesseract {
-
-#define BUCKETSIZE 16
 
 class OL_BUCKETS {
 public:
@@ -37,25 +36,13 @@ public:
       ICOORD bleft, // corners
       ICOORD tright);
 
-  ~OL_BUCKETS() = default;
-
   C_OUTLINE_LIST *operator()( // array access
       int16_t x,              // image coords
       int16_t y);
   // first non-empty bucket
-  C_OUTLINE_LIST *start_scan() {
-    for (index = 0; buckets[index].empty() && index < bxdim * bydim - 1; index++) {
-      ;
-    }
-    return &buckets[index];
-  }
+  C_OUTLINE_LIST *start_scan();
   // next non-empty bucket
-  C_OUTLINE_LIST *scan_next() {
-    for (; buckets[index].empty() && index < bxdim * bydim - 1; index++) {
-      ;
-    }
-    return &buckets[index];
-  }
+  C_OUTLINE_LIST *scan_next();
   int32_t count_children(     // recursive sum
       C_OUTLINE *outline,     // parent outline
       int32_t max_count);     // max output
@@ -68,12 +55,14 @@ public:
       C_OUTLINE_IT *it);      // destination iterator
 
 private:
-  std::unique_ptr<C_OUTLINE_LIST[]> buckets; // array of buckets
   int16_t bxdim;                             // size of array
   int16_t bydim;
+  std::vector<C_OUTLINE_LIST> buckets; // array of buckets
   ICOORD bl; // corners
   ICOORD tr;
-  int32_t index; // for extraction scan
+  decltype(buckets)::iterator it; // for extraction scan
+
+  C_OUTLINE_LIST *scan_next(decltype(buckets)::iterator it);
 };
 
 void extract_edges(Image pix,      // thresholded image

commit 4daf781916214fe7c8351724403b1d792ee1f8bd

Fix NULL pointer access (issue #3394)
The regression was caused by commit 57c90eee02866ec929d8cd42e2de73a04464479f.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/textord/edgblob.cpp
@@ -421,7 +421,11 @@ static void empty_buckets(BLOCK *block,       // block to scan
   C_OUTLINE_LIST outlines; // outlines in block
                            // iterator
   C_OUTLINE_IT out_it = &outlines;
-  C_OUTLINE_IT bucket_it = buckets->start_scan();
+  auto start_scan = buckets->start_scan();
+  if (start_scan == nullptr) {
+    return;
+  }
+  C_OUTLINE_IT bucket_it = start_scan;
   C_BLOB_IT good_blobs = block->blob_list();
   C_BLOB_IT junk_blobs = block->reject_blobs();
 