tesseract-ocr/tesseract

commit 5db92b26aa4cab45f3da6714328c2fcd80891441

Replace remaining GenericVector by std::vector for src/textord
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/textord/colfind.cpp
@@ -101,7 +101,9 @@ ColumnFinder::ColumnFinder(int gridsize, const ICOORD &bleft, const ICOORD &trig
 }
 
 ColumnFinder::~ColumnFinder() {
-  column_sets_.delete_data_pointers();
+  for (auto set : column_sets_) {
+    delete set;
+  }
   delete[] best_columns_;
   delete stroke_width_;
   delete input_blobs_win_;
@@ -552,7 +554,7 @@ bool ColumnFinder::MakeColumns(bool single_column) {
     bool good_only = true;
     do {
       for (int i = 0; i < gridheight_; ++i) {
-        ColPartitionSet *line_set = part_sets.get(i);
+        ColPartitionSet *line_set = part_sets.at(i);
         if (line_set != nullptr && line_set->LegalColumnCandidate()) {
           ColPartitionSet *column_candidate = line_set->Copy(good_only);
           if (column_candidate != nullptr)
@@ -590,7 +592,7 @@ bool ColumnFinder::MakeColumns(bool single_column) {
     ComputeMeanColumnGap(any_multi_column);
   }
   for (int i = 0; i < part_sets.size(); ++i) {
-    ColPartitionSet *line_set = part_sets.get(i);
+    ColPartitionSet *line_set = part_sets.at(i);
     if (line_set != nullptr) {
       line_set->RelinquishParts();
       delete line_set;
@@ -604,8 +606,9 @@ bool ColumnFinder::MakeColumns(bool single_column) {
 // Src_sets may be equal to column_candidates, in which case it will
 // use them as a source to improve themselves.
 void ColumnFinder::ImproveColumnCandidates(PartSetVector *src_sets, PartSetVector *column_sets) {
-  PartSetVector temp_cols;
-  temp_cols.move(column_sets);
+  // TODO: optimize.
+  PartSetVector temp_cols = *column_sets;
+  column_sets->clear();
   if (src_sets == column_sets)
     src_sets = &temp_cols;
   int set_size = temp_cols.size();
@@ -613,7 +616,7 @@ void ColumnFinder::ImproveColumnCandidates(PartSetVector *src_sets, PartSetVecto
   bool good_only = true;
   do {
     for (int i = 0; i < set_size; ++i) {
-      ColPartitionSet *column_candidate = temp_cols.get(i);
+      ColPartitionSet *column_candidate = temp_cols.at(i);
       ASSERT_HOST(column_candidate != nullptr);
       ColPartitionSet *improved = column_candidate->Copy(good_only);
       if (improved != nullptr) {
@@ -623,10 +626,15 @@ void ColumnFinder::ImproveColumnCandidates(PartSetVector *src_sets, PartSetVecto
     }
     good_only = !good_only;
   } while (column_sets->empty() && !good_only);
-  if (column_sets->empty())
-    column_sets->move(&temp_cols);
-  else
-    temp_cols.delete_data_pointers();
+  if (column_sets->empty()) {
+    // TODO: optimize.
+    column_sets = &temp_cols;
+    temp_cols.clear();
+  } else {
+    for (auto data : temp_cols) {
+      delete data;
+    }
+  }
 }
 
 // Prints debug information on the column candidates.
@@ -635,7 +643,7 @@ void ColumnFinder::PrintColumnCandidates(const char *title) {
   tprintf("Found %d %s:\n", set_size, title);
   if (textord_debug_tabfind >= 3) {
     for (int i = 0; i < set_size; ++i) {
-      ColPartitionSet *column_set = column_sets_.get(i);
+      ColPartitionSet *column_set = column_sets_.at(i);
       column_set->Print();
     }
   }
@@ -673,7 +681,7 @@ bool ColumnFinder::AssignColumns(const PartSetVector &part_sets) {
   // Set possible column_sets to indicate whether each set is compatible
   // with each column.
   for (int part_i = 0; part_i < set_count; ++part_i) {
-    ColPartitionSet *line_set = part_sets.get(part_i);
+    ColPartitionSet *line_set = part_sets.at(part_i);
     bool debug = line_set != nullptr && WithinTestRegion(2, line_set->bounding_box().left(),
                                                          line_set->bounding_box().bottom());
     column_set_costs[part_i] = new int[column_count];
@@ -681,8 +689,8 @@ bool ColumnFinder::AssignColumns(const PartSetVector &part_sets) {
     assigned_costs[part_i] = INT32_MAX;
     for (int col_i = 0; col_i < column_count; ++col_i) {
       if (line_set != nullptr &&
-          column_sets_.get(col_i)->CompatibleColumns(debug, line_set, WidthCB())) {
-        column_set_costs[part_i][col_i] = column_sets_.get(col_i)->UnmatchedWidth(line_set);
+          column_sets_.at(col_i)->CompatibleColumns(debug, line_set, WidthCB())) {
+        column_set_costs[part_i][col_i] = column_sets_.at(col_i)->UnmatchedWidth(line_set);
         any_columns_possible[part_i] = true;
       } else {
         column_set_costs[part_i][col_i] = INT32_MAX;
@@ -702,7 +710,7 @@ bool ColumnFinder::AssignColumns(const PartSetVector &part_sets) {
     int column_set_id = RangeModalColumnSet(column_set_costs, assigned_costs, start, end);
     if (textord_debug_tabfind >= 2) {
       tprintf("Range modal column id = %d\n", column_set_id);
-      column_sets_.get(column_set_id)->Print();
+      column_sets_.at(column_set_id)->Print();
     }
     // Now find the longest run of the column_set_id in the range.
     ShrinkRangeToLongestRun(column_set_costs, assigned_costs, any_columns_possible, column_set_id,
@@ -722,7 +730,7 @@ bool ColumnFinder::AssignColumns(const PartSetVector &part_sets) {
       tprintf("Column id %d applies to range = %d - %d\n", column_set_id, start, end);
     // Assign the column to the range, which now may overlap with other ranges.
     AssignColumnToRange(column_set_id, start, end, column_set_costs, assigned_costs);
-    if (column_sets_.get(column_set_id)->GoodColumnCount() > 1)
+    if (column_sets_.at(column_set_id)->GoodColumnCount() > 1)
       any_multi_column = true;
   }
   // If anything remains unassigned, the whole lot is unassigned, so
@@ -879,7 +887,7 @@ void ColumnFinder::ExtendRangePastSmallGaps(int **column_set_costs, const int *a
 // Assigns the given column_set_id to the given range.
 void ColumnFinder::AssignColumnToRange(int column_set_id, int start, int end,
                                        int **column_set_costs, int *assigned_costs) {
-  ColPartitionSet *column_set = column_sets_.get(column_set_id);
+  ColPartitionSet *column_set = column_sets_.at(column_set_id);
   for (int i = start; i < end; ++i) {
     assigned_costs[i] = column_set_costs[i][column_set_id];
     best_columns_[i] = column_set;

src/ccstruct/linlsq.h
@@ -2,7 +2,6 @@
  * File:        linlsq.h  (Formerly llsq.h)
  * Description: Linear Least squares fitting code.
  * Author:      Ray Smith
- * Created:     Thu Sep 12 08:44:51 BST 1991
  *
  * (C) Copyright 1991, Hewlett-Packard Ltd.
  ** Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,12 +21,10 @@
 
 #include "points.h" // for FCOORD
 
+#include <algorithm> // for std::nth_element
 #include <cstdint> // for int32_t
 
 namespace tesseract {
-
-template <typename T>
-class GenericVector;
 
 class TESS_API LLSQ {
 public:
@@ -111,29 +108,30 @@ private:
 // An assumption is made that most of the values are spread over no more than
 // half the range, but wrap-around is accounted for if the median is near
 // the wrap-around point.
-// Cannot be a member of GenericVector, as it makes heavy used of LLSQ.
+// Cannot be a member of vector, as it makes heavy use of LLSQ.
 // T must be an integer or float/double type.
 template <typename T>
-T MedianOfCircularValues(T modulus, GenericVector<T> *v) {
+T MedianOfCircularValues(T modulus, std::vector<T> &v) {
   LLSQ stats;
   T halfrange = static_cast<T>(modulus / 2);
-  int num_elements = v->size();
-  for (int i = 0; i < num_elements; ++i) {
-    stats.add((*v)[i], (*v)[i] + halfrange);
+  auto num_elements = v.size();
+  for (auto i : v) {
+    stats.add(i, i + halfrange);
   }
   bool offset_needed = stats.y_variance() < stats.x_variance();
   if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] += halfrange;
+    for (auto i : v) {
+      i += halfrange;
     }
   }
-  int median_index = v->choose_nth_item(num_elements / 2);
+  auto median_index = num_elements / 2;
+  std::nth_element(v.begin(), v.begin() + median_index, v.end());
   if (offset_needed) {
-    for (int i = 0; i < num_elements; ++i) {
-      (*v)[i] -= halfrange;
+    for (auto i : v) {
+      i -= halfrange;
     }
   }
-  return (*v)[median_index];
+  return v[median_index];
 }
 
 } // namespace tesseract

src/ccstruct/statistc.cpp
@@ -462,13 +462,13 @@ static bool GatherPeak(int index, const int *src_buckets, int *used_buckets, int
 // to sort on the output will re-sort by increasing mean of peak if that is
 // more useful than decreasing total count.
 // Returns the actual number of modes found.
-int STATS::top_n_modes(int max_modes, GenericVector<KDPairInc<float, int>> *modes) const {
+int STATS::top_n_modes(int max_modes, std::vector<KDPairInc<float, int>> &modes) const {
   if (max_modes <= 0)
     return 0;
   int src_count = rangemax_ - rangemin_;
   // Used copies the counts in buckets_ as they get used.
   STATS used(rangemin_, rangemax_);
-  modes->truncate(0);
+  modes.clear();
   // Total count of the smallest peak found so far.
   int least_count = 1;
   // Mode that is used as a seed for each peak
@@ -502,21 +502,21 @@ int STATS::top_n_modes(int max_modes, GenericVector<KDPairInc<float, int>> *mode
                         &total_value))
           break;
       }
-      if (total_count > least_count || modes->size() < max_modes) {
+      if (total_count > least_count || modes.size() < max_modes) {
         // We definitely want this mode, so if we have enough discard the least.
-        if (modes->size() == max_modes)
-          modes->truncate(max_modes - 1);
+        if (modes.size() == max_modes)
+          modes.resize(max_modes - 1);
         int target_index = 0;
         // Linear search for the target insertion point.
-        while (target_index < modes->size() && (*modes)[target_index].data() >= total_count)
+        while (target_index < modes.size() && modes[target_index].data() >= total_count)
           ++target_index;
         auto peak_mean = static_cast<float>(total_value / total_count + rangemin_);
-        modes->insert(KDPairInc<float, int>(peak_mean, total_count), target_index);
-        least_count = modes->back().data();
+        modes.insert(modes.begin() + target_index, KDPairInc<float, int>(peak_mean, total_count));
+        least_count = modes.back().data();
       }
     }
   } while (max_count > 0);
-  return modes->size();
+  return modes.size();
 }
 
 /**********************************************************************

src/ccstruct/statistc.h
@@ -113,7 +113,7 @@ public:
   // sort on the output will re-sort by increasing mean of peak if that is more
   // useful than decreasing total count. Returns the actual number of modes
   // found.
-  int top_n_modes(int max_modes, GenericVector<KDPairInc<float, int>> *modes) const;
+  int top_n_modes(int max_modes, std::vector<KDPairInc<float, int>> &modes) const;
 
   // Prints a summary and table of the histogram.
   void print() const;

src/ccutil/genericvector.h
@@ -245,25 +245,6 @@ public:
     return bottom;
   }
 
-  // Compact the vector by deleting elements using operator!= on basic types.
-  // The vector must be sorted.
-  void compact_sorted() {
-    if (size_used_ == 0) {
-      return;
-    }
-
-    // First element is in no matter what, hence the i = 1.
-    int last_write = 0;
-    for (int i = 1; i < size_used_; ++i) {
-      // Finds next unique item and writes it.
-      if (data_[last_write] != data_[i]) {
-        data_[++last_write] = data_[i];
-      }
-    }
-    // last_write is the index of a valid data cell, so add 1.
-    size_used_ = last_write + 1;
-  }
-
   // Returns the index of what would be the target_index_th item in the array
   // if the members were sorted, without actually sorting. Members are
   // shuffled around, but it takes O(n) time.

src/textord/baselinedetect.cpp
@@ -277,8 +277,8 @@ double BaselineRow::AdjustBaselineToGrid(int debug, const FCOORD &direction, dou
 void BaselineRow::SetupBlobDisplacements(const FCOORD &direction) {
   // Set of perpendicular displacements of the blob bottoms from the required
   // baseline direction.
-  GenericVector<double> perp_blob_dists;
-  displacement_modes_.truncate(0);
+  std::vector<double> perp_blob_dists;
+  displacement_modes_.clear();
   // Gather the skew-corrected position of every blob.
   double min_dist = FLT_MAX;
   double max_dist = -FLT_MAX;
@@ -310,8 +310,8 @@ void BaselineRow::SetupBlobDisplacements(const FCOORD &direction) {
   for (int i = 0; i < perp_blob_dists.size(); ++i) {
     dist_stats.add(IntCastRounded(perp_blob_dists[i] / disp_quant_factor_), 1);
   }
-  GenericVector<KDPairInc<float, int>> scaled_modes;
-  dist_stats.top_n_modes(kMaxDisplacementsModes, &scaled_modes);
+  std::vector<KDPairInc<float, int>> scaled_modes;
+  dist_stats.top_n_modes(kMaxDisplacementsModes, scaled_modes);
 #ifdef kDebugYCoord
   if (debug) {
     for (int i = 0; i < scaled_modes.size(); ++i) {
@@ -428,7 +428,7 @@ double BaselineBlock::SpacingModelError(double perp_disp, double line_spacing, d
 bool BaselineBlock::FitBaselinesAndFindSkew(bool use_box_bottoms) {
   if (non_text_block_)
     return false;
-  GenericVector<double> angles;
+  std::vector<double> angles;
   for (int r = 0; r < rows_.size(); ++r) {
     BaselineRow *row = rows_[r];
     if (row->FitBaseline(use_box_bottoms)) {
@@ -440,7 +440,7 @@ bool BaselineBlock::FitBaselinesAndFindSkew(bool use_box_bottoms) {
   }
 
   if (!angles.empty()) {
-    skew_angle_ = MedianOfCircularValues(M_PI, &angles);
+    skew_angle_ = MedianOfCircularValues(M_PI, angles);
     good_skew_angle_ = true;
   } else {
     skew_angle_ = 0.0f;
@@ -610,7 +610,7 @@ void BaselineBlock::DrawPixSpline(Pix *pix_in) {
 // observations.
 bool BaselineBlock::ComputeLineSpacing() {
   FCOORD direction(cos(skew_angle_), sin(skew_angle_));
-  GenericVector<double> row_positions;
+  std::vector<double> row_positions;
   ComputeBaselinePositions(direction, &row_positions);
   if (row_positions.size() < 2)
     return false;
@@ -644,7 +644,7 @@ bool BaselineBlock::ComputeLineSpacing() {
 // of the block baseline a line sits, hence the function and argument name
 // positions not distances.
 void BaselineBlock::ComputeBaselinePositions(const FCOORD &direction,
-                                             GenericVector<double> *positions) {
+                                             std::vector<double> *positions) {
   positions->clear();
   for (int r = 0; r < rows_.size(); ++r) {
     BaselineRow *row = rows_[r];
@@ -659,7 +659,7 @@ void BaselineBlock::ComputeBaselinePositions(const FCOORD &direction,
 // Computes an estimate of the line spacing of the block from the median
 // of the spacings between adjacent overlapping textlines.
 void BaselineBlock::EstimateLineSpacing() {
-  GenericVector<float> spacings;
+  std::vector<float> spacings;
   for (int r = 0; r < rows_.size(); ++r) {
     BaselineRow *row = rows_[r];
     // Exclude silly lines.
@@ -682,7 +682,8 @@ void BaselineBlock::EstimateLineSpacing() {
   // If we have at least one value, use it, otherwise leave the previous
   // value unchanged.
   if (!spacings.empty()) {
-    line_spacing_ = spacings[spacings.choose_nth_item(spacings.size() / 2)];
+    std::nth_element(spacings.begin(), spacings.begin() + spacings.size() / 2, spacings.end());
+    line_spacing_ = spacings[spacings.size() / 2];
     if (debug_level_ > 1)
       tprintf("Estimate of linespacing = %g\n", line_spacing_);
   }
@@ -692,7 +693,7 @@ void BaselineBlock::EstimateLineSpacing() {
 // line to the deskewed y-position of each baseline as a function of its
 // estimated line index, allowing for a small error in the initial linespacing
 // and choosing the best available model.
-void BaselineBlock::RefineLineSpacing(const GenericVector<double> &positions) {
+void BaselineBlock::RefineLineSpacing(const std::vector<double> &positions) {
   double spacings[3], offsets[3], errors[3];
   int index_range;
   errors[0] =
@@ -727,7 +728,7 @@ void BaselineBlock::RefineLineSpacing(const GenericVector<double> &positions) {
 // and the corresponding intercept in c_out, and the number of spacings seen
 // in index_delta. Returns the error of fit to the line spacing model.
 // Uses a simple linear regression, but optimized the offset using the median.
-double BaselineBlock::FitLineSpacingModel(const GenericVector<double> &positions, double m_in,
+double BaselineBlock::FitLineSpacingModel(const std::vector<double> &positions, double m_in,
                                           double *m_out, double *c_out, int *index_delta) {
   if (m_in == 0.0f || positions.size() < 2) {
     *m_out = m_in;
@@ -736,12 +737,12 @@ double BaselineBlock::FitLineSpacingModel(const GenericVector<double> &positions
       *index_delta = 0;
     return 0.0;
   }
-  GenericVector<double> offsets;
+  std::vector<double> offsets;
   // Get the offset (remainder) linespacing for each line and choose the median.
   for (int i = 0; i < positions.size(); ++i)
     offsets.push_back(fmod(positions[i], m_in));
   // Get the median offset.
-  double median_offset = MedianOfCircularValues(m_in, &offsets);
+  double median_offset = MedianOfCircularValues(m_in, offsets);
   // Now fit a line to quantized line number and offset.
   LLSQ llsq;
   int min_index = INT32_MAX;
@@ -755,7 +756,7 @@ double BaselineBlock::FitLineSpacingModel(const GenericVector<double> &positions
   // Get the refined line spacing.
   *m_out = llsq.m();
   // Use the median offset rather than the mean.
-  offsets.truncate(0);
+  offsets.clear();
   if (*m_out != 0.0) {
     for (int i = 0; i < positions.size(); ++i) {
       offsets.push_back(fmod(positions[i], *m_out));
@@ -766,7 +767,7 @@ double BaselineBlock::FitLineSpacingModel(const GenericVector<double> &positions
         tprintf("%d: %g\n", i, offsets[i]);
       }
     }
-    *c_out = MedianOfCircularValues(*m_out, &offsets);
+    *c_out = MedianOfCircularValues(*m_out, offsets);
   } else {
     *c_out = 0.0;
   }
@@ -808,7 +809,7 @@ BaselineDetect::BaselineDetect(int debug_level, const FCOORD &page_skew, TO_BLOC
 // block-wise and page-wise data to smooth small blocks/rows, and applies
 // smoothing based on block/page-level skew and block-level linespacing.
 void BaselineDetect::ComputeStraightBaselines(bool use_box_bottoms) {
-  GenericVector<double> block_skew_angles;
+  std::vector<double> block_skew_angles;
   for (int i = 0; i < blocks_.size(); ++i) {
     BaselineBlock *bl_block = blocks_[i];
     if (debug_level_ > 0)
@@ -820,7 +821,7 @@ void BaselineDetect::ComputeStraightBaselines(bool use_box_bottoms) {
   // Compute a page-wide default skew for blocks with too little information.
   double default_block_skew = page_skew_.angle();
   if (!block_skew_angles.empty()) {
-    default_block_skew = MedianOfCircularValues(M_PI, &block_skew_angles);
+    default_block_skew = MedianOfCircularValues(M_PI, block_skew_angles);
   }
   if (debug_level_ > 0) {
     tprintf("Page skew angle = %g\n", default_block_skew);

src/textord/baselinedetect.h
@@ -23,8 +23,6 @@
 #include "points.h"
 #include "rect.h"
 
-#include "genericvector.h"
-
 struct Pix;
 
 namespace tesseract {
@@ -109,7 +107,7 @@ private:
   FCOORD baseline_pt1_;
   FCOORD baseline_pt2_;
   // Set of modes of displacements. They indicate preferable baseline positions.
-  GenericVector<double> displacement_modes_;
+  std::vector<double> displacement_modes_;
   // Quantization factor used for displacement_modes_.
   double disp_quant_factor_;
   // Half the acceptance range of blob displacements for computing the
@@ -187,7 +185,7 @@ private:
 
   // Computes the deskewed vertical position of each baseline in the block and
   // stores them in the given vector.
-  void ComputeBaselinePositions(const FCOORD &direction, GenericVector<double> *positions);
+  void ComputeBaselinePositions(const FCOORD &direction, std::vector<double> *positions);
 
   // Computes an estimate of the line spacing of the block from the median
   // of the spacings between adjacent overlapping textlines.
@@ -197,13 +195,13 @@ private:
   // line to the deskewed y-position of each baseline as a function of its
   // estimated line index, allowing for a small error in the initial linespacing
   // and choosing the best available model.
-  void RefineLineSpacing(const GenericVector<double> &positions);
+  void RefineLineSpacing(const std::vector<double> &positions);
 
   // Given an initial estimate of line spacing (m_in) and the positions of each
   // baseline, computes the line spacing of the block more accurately in m_out,
   // and the corresponding intercept in c_out, and the number of spacings seen
   // in index_delta. Returns the error of fit to the line spacing model.
-  double FitLineSpacingModel(const GenericVector<double> &positions, double m_in, double *m_out,
+  double FitLineSpacingModel(const std::vector<double> &positions, double m_in, double *m_out,
                              double *c_out, int *index_delta);
 
   // The block to which this class adds extra information used during baseline

src/textord/bbgrid.h
@@ -384,6 +384,23 @@ int SortByBoxLeft(const void *void1, const void *void2) {
   return p1->bounding_box().top() - p2->bounding_box().top();
 }
 
+template <class BBC>
+bool StdSortByBoxLeft(const void *void1, const void *void2) {
+  // The void*s are actually doubly indirected, so get rid of one level.
+  const BBC *p1 = *static_cast<const BBC *const *>(void1);
+  const BBC *p2 = *static_cast<const BBC *const *>(void2);
+  int result = p1->bounding_box().left() - p2->bounding_box().left();
+  if (result != 0)
+    return result < 0;
+  result = p1->bounding_box().right() - p2->bounding_box().right();
+  if (result != 0)
+    return result < 0;
+  result = p1->bounding_box().bottom() - p2->bounding_box().bottom();
+  if (result != 0)
+    return result < 0;
+  return p1->bounding_box().top() < p2->bounding_box().top();
+}
+
 // Sort function to sort a BBC by bounding_box().right() in right-to-left order.
 template <class BBC>
 int SortRightToLeft(const void *void1, const void *void2) {
@@ -400,6 +417,23 @@ int SortRightToLeft(const void *void1, const void *void2) {
   if (result != 0)
     return result;
   return p1->bounding_box().top() - p2->bounding_box().top();
+}
+
+template <class BBC>
+bool StdSortRightToLeft(const void *void1, const void *void2) {
+  // The void*s are actually doubly indirected, so get rid of one level.
+  const BBC *p1 = *static_cast<const BBC *const *>(void1);
+  const BBC *p2 = *static_cast<const BBC *const *>(void2);
+  int result = p2->bounding_box().right() - p1->bounding_box().right();
+  if (result != 0)
+    return result < 0;
+  result = p2->bounding_box().left() - p1->bounding_box().left();
+  if (result != 0)
+    return result < 0;
+  result = p1->bounding_box().bottom() - p2->bounding_box().bottom();
+  if (result != 0)
+    return result < 0;
+  return p1->bounding_box().top() < p2->bounding_box().top();
 }
 
 // Sort function to sort a BBC by bounding_box().bottom().

commit 740d10b61be6009d4e523885b97168f8dd8c7a1c

Fix issue #3404 (empty page regression)
The regression was caused by a bug in commit 5db92b26aa4cab45f3da6714328c2fcd80891441.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/textord/colfind.cpp
@@ -641,7 +641,7 @@ void ColumnFinder::ImproveColumnCandidates(PartSetVector *src_sets, PartSetVecto
   } while (column_sets->empty() && !good_only);
   if (column_sets->empty()) {
     // TODO: optimize.
-    column_sets = &temp_cols;
+    *column_sets = temp_cols;
     temp_cols.clear();
   } else {
     for (auto data : temp_cols) {