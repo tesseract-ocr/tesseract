tesseract-ocr/tesseract

commit 4415209fd667722039c1a2ad71ef81a4f4854771

Remove tessopt. This fixes mastertrainer test in shared build.

src/training/common/commontraining.cpp
@@ -75,7 +75,6 @@ void ParseArguments(int* argc, char ***argv) {
 #include "params.h"
 #include "shapetable.h"
 #include "tessdatamanager.h"
-#include "tessopt.h"
 #include "tprintf.h"
 #include "unicity_table.h"
 
@@ -130,9 +129,6 @@ void ParseArguments(int* argc, char ***argv) {
   }
   usage += " [.tr files ...]";
   tesseract::ParseCommandLineFlags(usage.c_str(), argc, argv, true);
-  // Record the index of the first non-flag argument to 1, since we set
-  // remove_flags to true when parsing the flags.
-  tessoptind = 1;
   // Set some global values based on the flags.
   Config.MinSamples =
           std::max(0.0, std::min(1.0, double(FLAGS_clusterconfig_min_samples_fraction)));
@@ -253,7 +249,8 @@ std::unique_ptr<MasterTrainer> LoadTrainingData(int argc, const char* const * ar
   trainer->SetFeatureSpace(fs);
   const char* page_name;
   // Load training data from .tr files on the command line.
-  while ((page_name = GetNextFilename(argc, argv)) != nullptr) {
+  int tessoptind = 0;
+  while ((page_name = GetNextFilename(argc, argv, tessoptind)) != nullptr) {
     tprintf("Reading %s ...\n", page_name);
     trainer->ReadTrainingSamples(page_name, feature_defs, false);
 
@@ -319,7 +316,7 @@ std::unique_ptr<MasterTrainer> LoadTrainingData(int argc, const char* const * ar
  * - tessoptind defined by tessopt sys call
  * @return Next command line argument or nullptr.
  */
-const char *GetNextFilename(int argc, const char* const * argv) {
+const char *GetNextFilename(int argc, const char* const * argv, int &tessoptind) {
   if (tessoptind < argc)
     return argv[tessoptind++];
   else

src/training/CMakeLists.txt
@@ -59,13 +59,6 @@ if (NOT SW_BUILD)
     find_package(ICU 52.1 COMPONENTS uc i18n)
 endif()
 
-########################################
-# LIBRARY tessopt
-########################################
-
-add_library                 (tessopt STATIC tessopt.cpp tessopt.h)
-project_group               (tessopt "Training Tools")
-
 
 ########################################
 # LIBRARY common_training
@@ -101,7 +94,7 @@ endif()
 
 add_library                 (common_training ${common_training_src})
 target_include_directories  (common_training PUBLIC common ${CMAKE_CURRENT_BINARY_DIR})
-target_link_libraries       (common_training PUBLIC libtesseract tessopt)
+target_link_libraries       (common_training PUBLIC libtesseract)
 generate_export_header      (common_training EXPORT_MACRO_NAME TESS_COMMON_TRAINING_API)
 project_group               (common_training "Training Tools")
 

src/training/cntraining.cpp
@@ -120,7 +120,8 @@ int main(int argc, char *argv[]) {
 
   ParseArguments(&argc, &argv);
   int num_fonts = 0;
-  while ((PageName = GetNextFilename(argc, argv)) != nullptr) {
+  int tessoptind = 1;
+  while ((PageName = GetNextFilename(argc, argv, tessoptind)) != nullptr) {
     printf("Reading %s ...\n", PageName);
     FILE *TrainingPage = fopen(PageName, "rb");
     ASSERT_HOST(TrainingPage);

src/training/common/commontraining.h
@@ -127,7 +127,7 @@ std::unique_ptr<MasterTrainer> LoadTrainingData(int argc, const char* const * ar
 }  // namespace tesseract.
 
 TESS_COMMON_TRAINING_API
-const char *GetNextFilename(int argc, const char* const * argv);
+const char *GetNextFilename(int argc, const char* const * argv, int &tessoptind);
 
 LABELEDLIST FindList(
     tesseract::LIST        List,

src/training/tessopt.cpp
@@ -1,57 +1,0 @@
-/**********************************************************************
- * File:        tessopt.cpp
- * Description: Re-implementation of the unix code.
- * Author:      Ray Smith
- * Created:     Tue Nov 28 05:52:50 MST 1995
- *
- * (C) Copyright 1995, Hewlett-Packard Co.
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- ** http://www.apache.org/licenses/LICENSE-2.0
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- *
- **********************************************************************/
-
-#include "tessopt.h"
-
-#include <cstring>
-#include <cstdio>
-
-int tessoptind;
-
-/**********************************************************************
- * tessopt
- *
- * parse command line args.
- **********************************************************************/
-
-int tessopt(
-    int argc,
-    char *argv[],
-    const char *arglist  //string of arg chars
-) {
-  const char *arg;                     //arg char
-
-  if (tessoptind == 0)
-    tessoptind = 1;
-  if (tessoptind < argc && argv[tessoptind][0] == '-') {
-    arg = strchr (arglist, argv[tessoptind][1]);
-    if (arg == nullptr || *arg == ':')
-      return '?';                //dud option
-    tessoptind++;
-    if (arg[1] == ':') {
-      if (argv[tessoptind - 1][2] != '\0')
-        ;
-      else
-        tessoptind++;
-    }
-    return *arg;
-  }
-  else
-    return EOF;
-}

src/training/tessopt.h
@@ -1,31 +1,0 @@
-/**********************************************************************
- * File:        tessopt.h
- * Description: Re-implementation of the unix code.
- * Author:      Ray Smith
- *
- * (C) Copyright 1995, Hewlett-Packard Co.
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- ** http://www.apache.org/licenses/LICENSE-2.0
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- *
- **********************************************************************/
-
-#ifndef TESSERACT_TRAINING_TESSOPT_H_
-#define TESSERACT_TRAINING_TESSOPT_H_
-
-extern int tessoptind;
-
-//parse args
-int tessopt(
-    int argc,               // arg count
-    char *argv[],           // args
-    const char *arglist     // string of arg chars
-);
-
-#endif  // TESSERACT_TRAINING_TESSOPT_H_

sw.cpp
@@ -114,21 +114,13 @@ void build(Solution &s)
     auto &training = tess.addDirectory("training");
 
     //
-    auto &tessopt = training.addStaticLibrary("tessopt");
-    {
-        tessopt += cppstd;
-        tessopt += "src/training/tessopt.*"_rr;
-        tessopt.Public += libtesseract;
-    }
-
-    //
     auto &common_training = training.addLibrary("common_training");
     {
         common_training += "TESS_COMMON_TRAINING_API"_api;
         common_training += cppstd;
         common_training += "src/training/common/.*"_rr;
         common_training.Public += "src/training/common"_idir;
-        common_training.Public += tessopt;
+        common_training.Public += libtesseract;
     }
 
     //

unittest/mastertrainer_test.cc
@@ -32,7 +32,6 @@
 #include "shapetable.h"
 #include "trainingsample.h"
 #include "commontraining.h"
-#include "tessopt.h"                    // tessoptind
 
 #include "absl/strings/numbers.h"       // for safe_strto32
 #include "absl/strings/str_split.h"     // for absl::StrSplit
@@ -184,7 +183,6 @@ class MasterTrainerTest : public testing::Test {
     delete master_trainer_;
     delete shape_table_;
     shape_table_ = nullptr;
-    tessoptind = 0;
     master_trainer_ =
         LoadTrainingData(argc, argv, false, &shape_table_, &file_prefix);
     EXPECT_TRUE(master_trainer_ != nullptr);

commit d87e08f2668d56a886b961b9a5746dc1350987a2

Fix crash of shapeclustering (fixes #3564)
Fixes: 4415209fd667 ("Remove tessopt. This fixes mastertrainer test in shared build")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/training/common/commontraining.cpp
@@ -238,7 +238,7 @@ std::unique_ptr<MasterTrainer> LoadTrainingData(int argc, const char *const *arg
   trainer->SetFeatureSpace(fs);
   const char *page_name;
   // Load training data from .tr files on the command line.
-  int tessoptind = 0;
+  int tessoptind = 1;
   while ((page_name = GetNextFilename(argc, argv, tessoptind)) != nullptr) {
     tprintf("Reading %s ...\n", page_name);
     trainer->ReadTrainingSamples(page_name, feature_defs, false);