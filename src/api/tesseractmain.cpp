tesseract-ocr/tesseract

commit 1a6c298696160247cf1398eae8a3d29fd32df01d

Add new command line option --loglevel
By default some less important log messages are suppressed now.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/api/tesseractmain.cpp
@@ -27,6 +27,7 @@
 #endif
 #include <cstdlib> // for std::getenv
 #include <iostream>
+#include <map>    // for std::map
 #include <memory> // std::unique_ptr
 
 #include <allheaders.h>
@@ -238,6 +239,8 @@ static void PrintHelpExtra(const char *program) {
       "  --user-words PATH     Specify the location of user words file.\n"
       "  --user-patterns PATH  Specify the location of user patterns file.\n"
       "  --dpi VALUE           Specify DPI for input image.\n"
+      "  --loglevel LEVEL      Specify logging level. LEVEL can be\n"
+      "                        ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL or OFF.\n"
       "  -l LANG[+LANG]        Specify language(s) used for OCR.\n"
       "  -c VAR=VALUE          Set value for config variables.\n"
       "                        Multiple -c arguments are allowed.\n"
@@ -333,8 +336,10 @@ static void PrintLangsList(tesseract::TessBaseAPI &api) {
 }
 
 static void PrintBanner() {
-  tprintf("Tesseract Open Source OCR Engine v%s with Leptonica\n",
-          tesseract::TessBaseAPI::Version());
+  if (log_level <= 0) {
+    tprintf("Tesseract Open Source OCR Engine v%s with Leptonica\n",
+            tesseract::TessBaseAPI::Version());
+  }
 }
 
 /**
@@ -403,6 +408,27 @@ static bool ParseArgs(int argc, char **argv, const char **lang, const char **ima
     } else if (strcmp(argv[i], "--dpi") == 0 && i + 1 < argc) {
       *dpi = atoi(argv[i + 1]);
       ++i;
+    } else if (strcmp(argv[i], "--loglevel") == 0 && i + 1 < argc) {
+      // Allow the log levels which are used by log4cxx.
+      const std::string loglevel_string = argv[++i];
+      static const std::map<const std::string, int> loglevels {
+        {"ALL", INT_MIN},
+        {"TRACE", 5000},
+        {"DEBUG", 10000},
+        {"INFO", 20000},
+        {"WARN", 30000},
+        {"ERROR", 40000},
+        {"FATAL", 50000},
+        {"OFF", INT_MAX},
+      };
+      try {
+        auto loglevel = loglevels.at(loglevel_string);
+	log_level = loglevel;
+      } catch(std::out_of_range) {
+        // TODO: Allow numeric argument?
+	tprintf("Error, unsupported --loglevel %s\n", loglevel_string.c_str());
+        return false;
+      }
     } else if (strcmp(argv[i], "--user-words") == 0 && i + 1 < argc) {
       vars_vec->push_back("user_words_file");
       vars_values->push_back(argv[i + 1]);
@@ -670,7 +696,7 @@ int main(int argc, char **argv) {
   // first TessBaseAPI must be destructed, DawgCache must be the last object.
   tesseract::Dict::GlobalDawgCache();
 
-  tesseract::TessBaseAPI api;
+  TessBaseAPI api;
 
   api.SetOutputName(outputbase);
 

src/ccutil/tprintf.cpp
@@ -32,6 +32,8 @@ namespace tesseract {
 
 #define MAX_MSG_LEN 2048
 
+INT_VAR(log_level, INT_MAX, "Logging level");
+
 static STRING_VAR(debug_file, "", "File to send tprintf output to");
 
 // Trace printf

src/ccutil/tprintf.h
@@ -2,7 +2,6 @@
  * File:        tprintf.h
  * Description: Trace version of printf - portable between UX and NT
  * Author:      Phil Cheatle
- * Created:     Wed Jun 28 15:01:15 BST 1995
  *
  * (C) Copyright 1995, Hewlett-Packard Ltd.
  ** Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,9 +19,13 @@
 #ifndef TESSERACT_CCUTIL_TPRINTF_H
 #define TESSERACT_CCUTIL_TPRINTF_H
 
+#include "params.h" // for BOOL_VAR_H
 #include <tesseract/export.h> // for TESS_API
 
 namespace tesseract {
+
+// Disable some log messages by setting log_level > 0.
+extern INT_VAR_H(log_level);
 
 // Main logging function.
 extern TESS_API void tprintf( // Trace printf

src/lstm/lstmrecognizer.cpp
@@ -234,7 +234,9 @@ bool LSTMRecognizer::LoadDictionary(const ParamsVectors *params, const std::stri
   if (dict_->FinishLoad()) {
     return true; // Success.
   }
-  tprintf("Failed to load any lstm-specific dictionaries for lang %s!!\n", lang.c_str());
+  if (log_level <= 0) {
+    tprintf("Failed to load any lstm-specific dictionaries for lang %s!!\n", lang.c_str());
+  }
   delete dict_;
   dict_ = nullptr;
   return false;

commit 1f8835d7313dc11ddd4a3818f20c2599d5fa4c35

Fix compiler error in try / catch statement
Fixes: 1a6c29869616 ("Add new command line option --loglevel")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/api/tesseractmain.cpp
@@ -417,7 +417,7 @@ static bool ParseArgs(int argc, char **argv, const char **lang, const char **ima
       try {
         auto loglevel = loglevels.at(loglevel_string);
 	log_level = loglevel;
-      } catch(std::out_of_range) {
+      } catch(const std::out_of_range& e) {
         // TODO: Allow numeric argument?
 	tprintf("Error, unsupported --loglevel %s\n", loglevel_string.c_str());
         return false;