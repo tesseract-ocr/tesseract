tesseract-ocr/tesseract

commit c6b25f3b6e524abe023afbaa3b912ae32576ab9e

Add assertions in IntCastRounded
https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=39185 could be
caused by an integer overflow in IntCastRounded.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccutil/helpers.h
@@ -173,6 +173,8 @@ inline int DivRounded(int a, int b) {
 // Return a double cast to int with rounding.
 inline int IntCastRounded(double x) {
   assert(std::isfinite(x));
+  assert(x < INT_MAX);
+  assert(x > INT_MIN);
   return x >= 0.0 ? static_cast<int>(x + 0.5) : -static_cast<int>(-x + 0.5);
 }
 

commit 17e795aaae7d40dbcb7d3365835c2f55ecc6355d

Add missing include statement for INT_MIN, INT_MAX
Fixes: c6b25f3b6e52 ("Add assertions in IntCastRounded")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccutil/helpers.h
@@ -21,6 +21,7 @@
 #define TESSERACT_CCUTIL_HELPERS_H_
 
 #include <cassert>
+#include <climits> // for INT_MIN, INT_MAX
 #include <cmath> // std::isfinite
 #include <cstdio>
 #include <cstring>