a91ea10924aa44726ffc967992ded95c5662ad9f
Stefan Weil <sw@weilnetz.de>
2021-11-03 10:53:25 +0100

    Optimize function ApproximateOutline
    The compiler can now inline several functions which are
    only used in this compilation unit.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/ccstruct/polyaprx.h
+++ src/ccstruct/polyaprx.h
@@ -1,8 +1,7 @@
 /**********************************************************************
- * File:        polyaprx.h  (Formerly polygon.h)
+ * File:        polyaprx.h
  * Description: Code for polygonal approximation from old edgeprog.
  * Author:      Ray Smith
- * Created:     Thu Nov 25 11:42:04 GMT 1993
  *
  * (C) Copyright 1993, Hewlett-Packard Ltd.
  ** Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,26 +22,10 @@
 namespace tesseract {
 
 class C_OUTLINE;
-struct EDGEPT;
 struct TESSLINE;
 
 // convert a chain-coded input to the old OUTLINE approximation
 TESSLINE *ApproximateOutline(bool allow_detailed_fx, C_OUTLINE *c_outline);
-EDGEPT *edgesteps_to_edgepts( // convert outline
-    C_OUTLINE *c_outline,     // input
-    EDGEPT edgepts[]          // output is array
-);
-void fix2(         // polygonal approx
-    EDGEPT *start, /*loop to approimate */
-    int area);
-EDGEPT *poly2(       // second poly
-    EDGEPT *startpt, /*start of loop */
-    int area         /*area of blob box */
-);
-void cutline(              // recursive refine
-    EDGEPT *first,         /*ends of line */
-    EDGEPT *last, int area /*area of object */
-);
 
 } // namespace tesseract
 
