49715f4d27ec390320f385083720c82b42b20763
Amit D <amitdev2222@gmail.com>
2021-11-04 13:49:32 +0200

    pagesegmode_test.cc: Disable some code for disable legacy build (#3626)
    Co-authored-by: Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
    Co-authored-by: Stefan Weil <sw@weilnetz.de>

--- unittest/pagesegmode_test.cc
+++ unittest/pagesegmode_test.cc
@@ -97,8 +97,13 @@ TEST_F(PageSegModeTest, WordTest) {
     VerifyRectText(tesseract::PSM_SINGLE_WORD, "183", 1396, 218, 114, 102);
     // Test a random pair of words as a line
     VerifyRectText(tesseract::PSM_SINGLE_LINE, "What should", 237, 393, 256, 36);
+  #ifdef DISABLED_LEGACY_ENGINE
+    // Skip check as LSTM mode adds a space.
+    LOG(INFO) << "Skip `Whatshould` test in LSTM Mode\n";
+  #else
     // Test a random pair of words as a word
     VerifyRectText(tesseract::PSM_SINGLE_WORD, "Whatshould", 237, 393, 256, 36);
+  #endif
     // Test single block mode.
     VerifyRectText(tesseract::PSM_SINGLE_BLOCK, "both the\nfrom the", 237, 450, 172, 94);
     // But doesn't work in line or word mode.
