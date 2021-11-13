tesseract-ocr/tesseract

commit 9091055783ac8f0315015c9660d9e625540e0d5e

Fix loading of additional model files (issue #3635)
Modernize also a for loop statement.

Fixes: d6de055acf65 ("Set default language for tesseract only if required")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccmain/tessedit.cpp
@@ -297,9 +297,6 @@ int Tesseract::init_tesseract(const std::string &arg0, const std::string &textba
   std::vector<std::string> langs_not_to_load;
   ParseLanguageString(language, &langs_to_load, &langs_not_to_load);
 
-  // Set the basename, compute the data directory.
-  main_setup(arg0, textbase);
-
   for (auto *lang : sub_langs_) {
     delete lang;
   }
@@ -310,8 +307,7 @@ int Tesseract::init_tesseract(const std::string &arg0, const std::string &textba
   // Load the rest into sub_langs_.
   // A range based for loop does not work here because langs_to_load
   // might be changed in the loop when a new submodel is found.
-  for (size_t lang_index = 0; lang_index < langs_to_load.size(); ++lang_index) {
-    auto &lang_to_load = langs_to_load[lang_index];
+  for (auto &lang_to_load : langs_to_load) {
     if (!IsStrInList(lang_to_load, langs_not_to_load)) {
       const char *lang_str = lang_to_load.c_str();
       Tesseract *tess_to_init;
@@ -399,6 +395,9 @@ int Tesseract::init_tesseract_internal(const std::string &arg0, const std::strin
                                        const std::vector<std::string> *vars_vec,
                                        const std::vector<std::string> *vars_values,
                                        bool set_only_non_debug_params, TessdataManager *mgr) {
+  // Set the basename, compute the data directory.
+  main_setup(arg0, textbase);
+
   if (!init_tesseract_lang_data(arg0, language, oem, configs, configs_size, vars_vec,
                                 vars_values, set_only_non_debug_params, mgr)) {
     return -1;

commit c2ee0cd06f4e2442a75a2ae30992159aa02eae06

Fix listing of languages
The last fix for OCR with more than one model introduced
a regression for `tesseract --list-langs`.

Fixes: 9091055783ac ("Fix loading of additional model files")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccmain/tessedit.cpp
@@ -300,6 +300,10 @@ int Tesseract::init_tesseract(const std::string &arg0, const std::string &textba
   for (auto *lang : sub_langs_) {
     delete lang;
   }
+
+  // Set the basename, compute the data directory.
+  main_setup(arg0, textbase);
+
   sub_langs_.clear();
   // Find the first loadable lang and load into this.
   // Add any languages that this language requires
@@ -315,6 +319,7 @@ int Tesseract::init_tesseract(const std::string &arg0, const std::string &textba
         tess_to_init = this;
       } else {
         tess_to_init = new Tesseract;
+        tess_to_init->main_setup(arg0, textbase);
       }
 
       int result = tess_to_init->init_tesseract_internal(arg0, textbase, lang_str, oem, configs,
@@ -395,9 +400,6 @@ int Tesseract::init_tesseract_internal(const std::string &arg0, const std::strin
                                        const std::vector<std::string> *vars_vec,
                                        const std::vector<std::string> *vars_values,
                                        bool set_only_non_debug_params, TessdataManager *mgr) {
-  // Set the basename, compute the data directory.
-  main_setup(arg0, textbase);
-
   if (!init_tesseract_lang_data(arg0, language, oem, configs, configs_size, vars_vec,
                                 vars_values, set_only_non_debug_params, mgr)) {
     return -1;