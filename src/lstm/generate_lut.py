7277963e11b6ca8710117ea9c7620b9b008e2c59
Stefan Weil <sw@weilnetz.de>
2021-12-27 10:31:42 +0100

    Update generator for lookup tables to use TFloat instead of double
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/lstm/generate_lut.py
+++ src/lstm/generate_lut.py
@@ -4,22 +4,24 @@
 
 import math
 
+# kTableSize and kScaleFactor must match the values in functions.h.
+
 # Size of static tables.
 kTableSize = 4096
 # Scale factor for float arg to int index.
 kScaleFactor = 256.0
 
-print("// Generated code with lookup tables")
+print("// Generated code with lookup tables (see generate_lut.py)")
 print('#include "functions.h"')
 print("namespace tesseract {")
 
-print("const double TanhTable[] = {")
+print("const TFloat TanhTable[] = {")
 for i in range(kTableSize):
-    print("  %a," % math.tanh(i / kScaleFactor))
+    print("    %a," % math.tanh(i / kScaleFactor))
 print("};")
 
-print("const double LogisticTable[] = {")
+print("const TFloat LogisticTable[] = {")
 for i in range(kTableSize):
-    print("  %a," % (1 / (1 + math.exp(-i / kScaleFactor))))
+    print("    %a," % (1 / (1 + math.exp(-i / kScaleFactor))))
 print("};")
-print("}  // namespace tesseract.")
+print("} // namespace tesseract.")
