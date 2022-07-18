tesseract-ocr/tesseract

commit 37b33749da56f5762346e16d0cc843460caacdb5

ScrollView: Fix memory leak and modernize code
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/viewer/scrollview.cpp
@@ -33,6 +33,7 @@
 #include <cstdarg>
 #include <cstring>
 #include <map>
+#include <memory> // for std::unique_ptr
 #include <mutex> // for std::mutex
 #include <string>
 #include <thread> // for std::thread
@@ -97,7 +98,7 @@ void ScrollView::MessageReceiver() {
   // the events accordingly.
   while (true) {
     // The new event we create.
-    auto *cur = new SVEvent;
+    std::unique_ptr<SVEvent> cur(new SVEvent);
     // The ID of the corresponding window.
     int window_id;
 
@@ -143,11 +144,12 @@ void ScrollView::MessageReceiver() {
 
       // In case of an SVET_EXIT event, quit the whole application.
       if (ev_type == SVET_EXIT) {
-        ScrollView::Exit();
+        SendRawMessage("svmain:exit()");
+        break;
       }
 
       // Place two copies of it in the table for the window.
-      cur->window->SetEvent(cur);
+      cur->window->SetEvent(cur.get());
 
       // Check if any of the threads currently waiting want it.
       std::pair<ScrollView *, SVEventType> awaiting_list(cur->window, cur->type);
@@ -156,17 +158,14 @@ void ScrollView::MessageReceiver() {
                                                                     SVET_ANY);
       waiting_for_events_mu->lock();
       if (waiting_for_events.count(awaiting_list) > 0) {
-        waiting_for_events[awaiting_list].second = cur;
+        waiting_for_events[awaiting_list].second = cur.get();
         waiting_for_events[awaiting_list].first->Signal();
       } else if (waiting_for_events.count(awaiting_list_any) > 0) {
-        waiting_for_events[awaiting_list_any].second = cur;
+        waiting_for_events[awaiting_list_any].second = cur.get();
         waiting_for_events[awaiting_list_any].first->Signal();
       } else if (waiting_for_events.count(awaiting_list_any_window) > 0) {
-        waiting_for_events[awaiting_list_any_window].second = cur;
+        waiting_for_events[awaiting_list_any_window].second = cur.get();
         waiting_for_events[awaiting_list_any_window].first->Signal();
-      } else {
-        // No one wanted it, so delete it.
-        delete cur;
       }
       waiting_for_events_mu->unlock();
       // Signal the corresponding semaphore twice (for both copies).
@@ -175,8 +174,6 @@ void ScrollView::MessageReceiver() {
         sv->Signal();
         sv->Signal();
       }
-    } else {
-      delete cur; // Applied to no window.
     }
     svmap_mu->unlock();
 

commit 4f831ff48967c6a8b7f927051bda1b432e105772

viewer: Fix double free caused by ScrollView::MessageReceiver
waiting_for_events takes ownership of the passed event which is later
deleted. Since we use unique_ptr::get() to acquire the pointer, we cause
double free: one free happens in the code path where the event from
waiting_for_events goes and the other free happens in unique_ptr
destructor.

The fix is to move ownership out of unique_ptr by unique_ptr::release().

Fixes: https://github.com/tesseract-ocr/tesseract/issues/3869
Fixes: 37b33749da56f5762346e16d0cc843460caacdb5

src/viewer/scrollview.cpp
@@ -158,13 +158,13 @@ void ScrollView::MessageReceiver() {
                                                                     SVET_ANY);
       waiting_for_events_mu->lock();
       if (waiting_for_events.count(awaiting_list) > 0) {
-        waiting_for_events[awaiting_list].second = cur.get();
+        waiting_for_events[awaiting_list].second = cur.release();
         waiting_for_events[awaiting_list].first->Signal();
       } else if (waiting_for_events.count(awaiting_list_any) > 0) {
-        waiting_for_events[awaiting_list_any].second = cur.get();
+        waiting_for_events[awaiting_list_any].second = cur.release();
         waiting_for_events[awaiting_list_any].first->Signal();
       } else if (waiting_for_events.count(awaiting_list_any_window) > 0) {
-        waiting_for_events[awaiting_list_any_window].second = cur.get();
+        waiting_for_events[awaiting_list_any_window].second = cur.release();
         waiting_for_events[awaiting_list_any_window].first->Signal();
       }
       waiting_for_events_mu->unlock();