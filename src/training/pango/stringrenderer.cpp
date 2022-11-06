4ab09a63b24bb2c45e2d1f15f72b07e0feff03e9
zdenop <zdenop@gmail.com>
2022-11-06 17:43:53 +0100

    fix typo in variable

--- src/training/pango/stringrenderer.cpp
+++ src/training/pango/stringrenderer.cpp
@@ -609,7 +609,7 @@ void StringRenderer::CorrectBoxPositionsToLayout(std::vector<BoxChar *> *boxchar
 
 int StringRenderer::StripUnrenderableWords(std::string *utf8_text) const {
   std::string output_text;
-  std::string unrendable_words;
+  std::string unrenderable_words;
   const char *text = utf8_text->c_str();
   size_t offset = 0;
   int num_dropped = 0;
@@ -626,15 +626,15 @@ int StringRenderer::StripUnrenderableWords(std::string *utf8_text) const {
       output_text.append(text + offset, word_len);
     } else {
       ++num_dropped;
-      unrendable_words.append(text + offset, word_len);
-      unrendable_words.append(" ");
+      unrenderable_words.append(text + offset, word_len);
+      unrenderable_words.append(" ");
     }
     offset += word_len;
   }
   utf8_text->swap(output_text);
 
   if (num_dropped > 0) {
-    tprintf("Stripped %d unrenderable word(s): '%s'\n", num_dropped, unrendable_words.c_str());
+    tprintf("Stripped %d unrenderable word(s): '%s'\n", num_dropped, unrenderable_words.c_str());
   }
   return num_dropped;
 }
