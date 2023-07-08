cf235ccfed99c584a335b5be0f083cd58caf6b0a
luzpaz <luzpaz@users.noreply.github.com>
2023-07-08 01:15:04 +0000

    Fix typos
    Found via `codespell -q 3 -L bu,eiter,fo,fpr,inout,numer,pixes,thisy`

--- src/textord/edgblob.cpp
+++ src/textord/edgblob.cpp
@@ -174,7 +174,7 @@ int32_t OL_BUCKETS::outline_complexity(C_OUTLINE *outline, // parent outline
         if (child_count + grandchild_count > max_count) { // too complex
           if (edges_debug) {
             tprintf(
-                "Disgard outline on child_count=%d + grandchild_count=%d "
+                "Discard outline on child_count=%d + grandchild_count=%d "
                 "> max_count=%d\n",
                 child_count, grandchild_count, max_count);
           }
