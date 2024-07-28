tesseract-ocr/tesseract

commit bc490ea7ab67d944781b2f3c2c1e99cce9d26abc

Ignore illegal TESSDATA_PREFIX (not existing filesystem entry, issue #4277)
Don't check for a directory, because a symbolic link is also allowed.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccutil/ccutil.cpp
@@ -17,7 +17,8 @@
 #include "ccutil.h"
 
 #include <cstdlib>
-#include <cstring> // for std::strrchr
+#include <cstring>    // for std::strrchrA
+#include <filesystem> // for std::filesystem
 
 namespace tesseract {
 
@@ -47,6 +48,12 @@ void CCUtil::main_setup(const std::string &argv0, const std::string &basename) {
   imagebasename = basename; /**< name of image */
 
   const char *tessdata_prefix = getenv("TESSDATA_PREFIX");
+
+  // Ignore TESSDATA_PREFIX if there is no matching filesystem entry.
+  if (tessdata_prefix != nullptr && !std::filesystem::exists(tessdata_prefix)) {
+    tprintf("Warning: TESSDATA_PREFIX %s does not exist, ignore it\n", tessdata_prefix);
+    tessdata_prefix = nullptr;
+  }
 
   if (!argv0.empty()) {
     /* Use tessdata prefix from the command line. */

commit c5030ea15a7f5303611317348b87b7329726f7cf

Add missing include statement
Fixes: bc490ea7ab67d944 ("Ignore illegal TESSDATA_PREFIX [...]")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/ccutil/ccutil.cpp
@@ -15,6 +15,7 @@
 #endif
 
 #include "ccutil.h"
+#include "tprintf.h"  // for tprintf
 
 #include <cstdlib>
 #include <cstring>    // for std::strrchrA