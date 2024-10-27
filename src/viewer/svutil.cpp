49cbe2b47d7cafe64a8c051d9aa7d9918d4e8c73
Stefan Weil <sw@weilnetz.de>
2024-10-27 07:09:32 +0100

    Fix compiler warning for argument of getaddrinfo
    Fix this clang warning:
    
        src/viewer/svutil.cpp:277:51:
          warning: missing field 'ai_protocol' initializer [-Wmissing-field-initializers]
    
    Replace also PF_INET by AF_INET which is the recommended value.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/viewer/svutil.cpp
+++ src/viewer/svutil.cpp
@@ -273,8 +273,6 @@ SVNetwork::SVNetwork(const char *hostname, int port) {
 
   buffer_ptr_ = nullptr;
 
-  struct addrinfo *addr_info = nullptr;
-  struct addrinfo hints = {0, PF_INET, SOCK_STREAM};
   auto port_string = std::to_string(port);
 #  ifdef _WIN32
   // Initialize Winsock
@@ -285,6 +283,10 @@ SVNetwork::SVNetwork(const char *hostname, int port) {
   }
 #  endif // _WIN32
 
+  struct addrinfo *addr_info = nullptr;
+  struct addrinfo hints = {};
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
   if (getaddrinfo(hostname, port_string.c_str(), &hints, &addr_info) != 0) {
     std::cerr << "Error resolving name for ScrollView host "
               << std::string(hostname) << ":" << port << std::endl;
