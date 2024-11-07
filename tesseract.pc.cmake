daaa902a5e78e9ff8f64262a4849d44d02539119
Stefan Weil <sw@weilnetz.de>
2024-11-07 11:39:00 +0100

    Update documentation on history of development
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- tesseract.pc.cmake
+++ tesseract.pc.cmake
@@ -4,7 +4,7 @@ libdir=@CMAKE_INSTALL_FULL_LIBDIR@
 includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
 
 Name: @tesseract_NAME@
-Description: An OCR Engine that was developed at HP Labs between 1985 and 1995... and now at Google.
+Description: An OCR Engine that was developed at HP Labs (1985-1995) and Google (2006-2018).
 URL: https://github.com/tesseract-ocr/tesseract
 Version: @tesseract_VERSION@
 Requires.private: lept
