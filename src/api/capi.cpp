992b4e72ea3c201f2df0a3b297966049fa99185c
Viktor Szépe <viktor@szepe.net>
2026-01-01 15:38:04 +0000

    Revert

--- src/api/capi.cpp
+++ src/api/capi.cpp
@@ -121,7 +121,7 @@ BOOL TessResultRendererEndDocument(TessResultRenderer *renderer) {
   return static_cast<int>(renderer->EndDocument());
 }
 
-const char *TessResultRendererExtension(TessResultRenderer *renderer) {
+const char *TessResultRendererExtention(TessResultRenderer *renderer) {
   return renderer->file_extension();
 }
 
