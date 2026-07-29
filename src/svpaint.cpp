3ca925bdecdb8559849602b14de5e4bf5fa6da2f
Egor Pugin <egor.pugin@gmail.com>
2026-07-29 18:43:41 +0300

    Fix build.

--- src/svpaint.cpp
+++ src/svpaint.cpp
@@ -105,7 +105,7 @@ SVMenuNode *SVPaint::BuildMenuBar() {
 void SVPaint::PopupHandler(const SVEvent *sv_event) {
   // Since we only have the RGB values as popup items,
   // we take a shortcut to not bloat up code:
-  rgb[sv_event->command_id - 1] = atoi(sv_event->parameter);
+  rgb[sv_event->command_id - 1] = std::stoi(sv_event->parameter);
   window_->Pen(rgb[0], rgb[1], rgb[2]);
 }
 
